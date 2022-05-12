1c2a0ce3b9d58fe409058d9fb7778e5b5c0cdcc3
JordyVerhoeven <33899722+JordyVerhoeven@users.noreply.github.com>
2022-05-12 13:00:21 +0200

    Implemented landing page (buttons still non functional). Some colors still need to be modified.

--- WriterDesk1/src/index.js
+++ WriterDesk1/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './pages/App.js';
+import LoginSignUp from './pages/LoginSignUp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
