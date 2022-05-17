744f38f79f60c316ad2f5e0c83ac7a395e6883ad
PFvanBeerendonk <64134830+PFvanBeerendonk@users.noreply.github.com>
2022-05-17 13:21:21 +0200

    fixed error last commit

--- WriterDesk1/src/pages/Upload.js
+++ WriterDesk1/src/pages/Upload.js
@@ -2,7 +2,6 @@ import { useOutletContext } from 'react-router-dom';
 import { useEffect } from 'react';
 
 
-
 /**
  * 
  * @returns Upload Page
@@ -11,6 +10,17 @@ import Typography from "@mui/material/Typography";
 import {Button, TextField} from "@mui/material";
 import {useState} from "react";
 
+const FileUpload = () => {
+    return(
+        <div className='vertCenter'>
+            <div className='upload'>
+                <button className='innerUploadButton'>Choose a file</button>
+                or drag it here.
+            </div>
+            <TextField label='dd/mm/yy' variant='outlined'/>
+            <TextField label='course' variant='outlined'/>
+        </div>
+)};
 
 const Upload = () => {
     //set title in parent 'base'
@@ -21,8 +31,8 @@ const Upload = () => {
 
     const [rowList, setRowList] = useState([]);
 
-    const addRow = event => {
-        setRowList(rowList.concat(<fileUpload key={rowList.length}/>));
+    const addRow = e => {
+        setRowList(rowList.concat(<FileUpload key={rowList.length} />));
     };
 
     return (
@@ -30,25 +40,12 @@ const Upload = () => {
             <div className='title'>
                 <Typography variant='h3'>Upload</Typography>
             </div>
-            {fileUpload()}
+            {FileUpload()}
             <div id='uploadRows'></div>
             {rowList}
             <Button onClick={addRow}>add</Button>
         </>
     );
 }
-
-const fileUpload = () => {
-    return <div>
-        <div className='vertCenter'>
-            <div className='upload'>
-                <button className='innerUploadButton'>Choose a file</button>
-                or drag it here.
-            </div>
-            <TextField label='dd/mm/yy' variant='outlined'/>
-            <TextField label='course' variant='outlined'/>
-        </div>
-    </div>
-};
 
 export default Upload;
