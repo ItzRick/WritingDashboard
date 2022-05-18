05c51de7c0f87bd0330b7562a53937494a0774a0
RIck Overmeer <r.j.overmeer@student.tue.nl>
2022-05-18 15:57:22 +0200

    Added drag and drop

--- WriterDesk1/src/components/UploadSingleFile.js
+++ WriterDesk1/src/components/UploadSingleFile.js
@@ -1,21 +1,59 @@
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { useEffect, useState, useRef } from 'react';
 // materials
 import {
     Button,
     TextField
 } from "@mui/material";
 
+
+
 /**
  * 
  * @returns Single File Upload Object
  */
  const UploadSingleFile = () => {
+
+    const [file, setFile] = useState('or drag it here.');
+
+    const onFileChange = (event) => {
+        if (event.target.files[0] === undefined) {
+            setFile('or drag it here.')
+        } else {
+            setFile(event.target.files[0]);
+        }
+    }
+
+    const onFileDrop = (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        let dt = event.dataTransfer;
+        let files = dt.files;
+        console.log(files[0]);
+        setFile(files[0]);
+    }
+
+    const fileInput = useRef();
     return(
         <div style={{marginBottom: '1vw'}}>
             <div className='vertCenter'>
-                <div className='upload'>
-                    <Button variant='contained' sx={{mr: '8px', bgcolor: 'button.main', color: 'button.text'}}>Choose a file</Button>
-                    or drag it here.
+                <div className='upload'
+                onDragEnter={(event)=>event.preventDefault()}
+                onDragOver={(event)=>event.preventDefault()}
+                onDrop = {onFileDrop}>
+                    <Button variant='contained' sx={{mr: '8px', bgcolor: 'button.main', color: 'button.text'}} onClick={()=>fileInput.current.click()}>Choose a file</Button>
+                    <input 
+                        ref={fileInput} 
+                        type="file" 
+                        style={{ display: 'none' }}
+                        onChange={onFileChange} 
+                    />
+                   {file.name !== undefined ? file.name : 'or drag it here.'}
                 </div>
+                
                 <TextField label='dd/mm/yy' variant='outlined' style={{marginRight: '1vw'}}/>
                 <TextField label='course' variant='outlined'/>
             </div>
--- WriterDesk1/src/pages/Upload.js
+++ WriterDesk1/src/pages/Upload.js
@@ -1,88 +1,33 @@
-import React, {useState, useRef} from 'react';
-import axios from 'axios';
-import useDynamicRefs from 'use-dynamic-refs';
+// materials
+import { 
+    Typography,
+    Button
+} from "@mui/material";
+import UploadSingleFile from "./../components/UploadSingleFile";
 
-import '../App.css';
 
+// routing
+import { useOutletContext } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
+/**
+ * 
+ * @returns File Upload page
+ */
 const Upload = () => {
-    const onFileChange = (index, event) => {
-        console.log(event.target.files[0].type);
-        const fileType = event.target.files[0].type
-        const isPDF = fileType === 'application/pdf';
-        const isWord1 = fileType === ('application/vnd.openxmlformats-officedocument.wordprocessingml.document');
-        const isWord2 = fileType === 'application/msword';
-        const isTXT = fileType === 'text/plain';
-        let data = [...files];
-        if (!(isPDF || isWord1 || isWord2 || isTXT)) {
-            // console.log();
-            data[index] = '';
-            reset();
-            console.log(files);
-            setError(index);
-            setReceivedFileType(fileType);
-        } else {
-            data[index] = event.target.files[0];
-            setError(-1);
-        }
-        setFiles(data);
+    //set title in parent 'base'
+    const { setTitle } = useOutletContext();
+    useEffect(() => {
+        setTitle('Upload');
+    });
+
+    // list of UploadSingleFile objects
+    const [rowList, setRowList] = useState([]);
+
+    // add UploadSingleFile object to rowList
+    const addRow = e => {
+        setRowList(rowList.concat(<UploadSingleFile key={rowList.length} />));
     };
-
-    const addFileSelector = () => {
-        setFiles([...files, ''])
-        // console.log(files)
-    }
-
-    const removeFileSelector = (index) => {
-        let currentFiles = [...files]
-        currentFiles.splice(index, 1)
-        setFiles(currentFiles)
-        // console.log(files)
-    }
-
-    const submitFiles = event => {
-        event.preventDefault();
-        console.log(files[0]);
-        // Connnection to the backend, URL to be changed later:
-        const url = 'https://localhost:5000/fileapi/upload';
-        const formData = new FormData();
-        // formData.append('files', files[0])
-        files.forEach(file => formData.append('files',file));
-        // formData.append('files', files);
-        console.log(formData);
-        files.forEach(file => formData.append('fileName', file.name));
-        files.forEach(file => formData.append('userId', 123));
-        const headers = {
-            Accept: 'application/json',
-            'Content-Type': 'multipart/form-data',
-        }
-        axios.post(url, formData, headers).then((response) => {
-            console.log(response.data)
-            if (response.data === 'success') {
-                let oldLength = files.length;
-                // Reset the upload selectors to not have a file displayed:
-                setFiles(['']);
-                files.forEach((_file, index) => {
-                    reset(index);
-                }
-                )
-            } 
-        });
-      
-        
-    };
-
-    const [receivedFileType, setReceivedFileType] = useState('');
-    const [hasError, setError] = useState(-1);
-
-    // To be able to reset the things at the end:
-    const [getRef, setRef] =  useDynamicRefs();
-
-    const reset = (index) => {
-        getRef(index.toString()).current.value = "";
-    };
-
-    const [files, setFiles] = useState(['']);
 
     return (
         <>
@@ -104,6 +49,5 @@ const Upload = () => {
         </>
     );
 }
-
 
 export default Upload;
