23771957f0a9d96f0090102d3657c74a4ddfeb65
JKuppen <48519274+JKuppen@users.noreply.github.com>
2022-05-19 13:38:01 +0200

    Started with displaying uploaded documents as pdf

--- WriterDesk1/src/pages/Documents.js
+++ WriterDesk1/src/pages/Documents.js
@@ -6,22 +6,30 @@ import { useOutletContext } from 'react-router-dom';
 import { useEffect } from 'react';
 
 
-/**
- * 
- * @returns Documents Page
- */
-function Documents() {
-  //set title in parent 'base' 
-  const { setTitle } = useOutletContext();
-  useEffect(() => {
-    setTitle('Documents');
-  });
-  
+import React from 'react';
+import AllPagesPDFViewer from "../components/all-pages";
+import samplePDF from "../example2.pdf";
+import "../css/styles.css";
+
+
+// /**
+//  * 
+//  * @returns Documents Page
+//  */
+// function Documents() {
+//   //set title in parent 'base' 
+//   const { setTitle } = useOutletContext();
+//   useEffect(() => {
+//     setTitle('Documents');
+//   });
+export default function Documents() {  
   return (
-    <>
-      Documents
-    </>
+    <div className="App">
+      <div className="all-page-container">
+        <AllPagesPDFViewer pdf={samplePDF} />
+      </div>
+    </div>
   );
 }
 
-export default Documents;
+//export default Documents;
