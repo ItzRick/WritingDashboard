7279c12822326740073b4efdd0abe696a88f28ee
JordyVerhoeven <33899722+JordyVerhoeven@users.noreply.github.com>
2022-05-19 15:21:44 +0200

    Created separate document page

---
+++ WriterDesk1/src/pages/Document.js
@@ -0,0 +1,44 @@
+// materials
+import {Typography} from "@mui/material";
+
+// routing
+import { useOutletContext } from 'react-router-dom';
+import { useEffect } from 'react';
+
+
+import React from 'react';
+import AllPagesPDFViewer from "../components/all-pages";
+import samplePDF from "../example2.pdf";
+import "../css/styles.css";
+import "../css/main.css";
+import placeholder from '../images/chartImage.png';
+
+
+/**
+ *
+ * @returns Documents Page
+ */
+function Document() {
+  //set title in parent 'base'
+  const { setTitle } = useOutletContext();
+  useEffect(() => {
+    setTitle('Document');
+  });
+  return (
+      <>
+          <div className="pdfContainer">
+            <AllPagesPDFViewer pdf={samplePDF} />
+          </div>
+          <div className='rightFloat'>
+              <img className='smallGraph' src={placeholder} />
+              <br />
+              <div className='textBoxExpl'>
+                  <Typography>Sample text</Typography>
+              </div>
+          </div>
+      </>
+
+  );
+}
+
+export default Documents;
