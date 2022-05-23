6b2037ee51071d4a9b50377faad3809cac4483a8
RIck Overmeer <r.j.overmeer@student.tue.nl>
2022-05-23 13:44:35 +0200

    comments

--- WriterDesk1/src/pages/Document.js
+++ WriterDesk1/src/pages/Document.js
@@ -32,32 +32,41 @@ function Document() {
   const type = 'docx'
   return (
       <>
+        {/* Div for the pdf file: */}
           <div className="allPageContainer" >
             <AllPagesPDFViewer pdf={`http://127.0.0.1:5000/converttopdf/convert?filepath=${path}&filetype=${type}`} />
           </div>
+          {/* Div for the sidebar: */}
           <div className='sideBar'>
+              {/* The actual barchart, with the colors as required by the design and score as set in the currentData state: */}
             <Plot
                 data={[
                     {
+                        // Order of the bars is as follows: first source integration, then cohesion, then structure, then language & style:
                     x: ['Source integration & <br> content', 'Cohesion', 'Structure', "Language & style"],
                     y: currentData,
                     marker: {color: ['#F5793A', '#A95AA1', '#85C0F9', '#0F2080']},
                     type: 'bar',
                     },
                 ]}
-                layout={ {title: 'Scores', 
+                // The title of the char is 'scores':
+                layout={ {title: 'Scores',
+                // Scores can be between 0 and 10, so the y-axis range is set accordingly: 
                 yaxis: {
                     range: [0, 10],
                     type: 'linear'
                 }} }
+                // Do not display the plotly modebar:
                 config= {{
                     displayModeBar: false, // this is the line that hides the bar.
                 }}
+                // So that you can do stuff if you click on a bar (TODO: remove):
                 onClick={(data) => {
                     console.log("data", data.points[0].pointNumber)
                 }}
+                // So the chart can resize:
                 useResizeHandler={true}
-                style={{width: '40vw', height: '50vh'}}
+                style={{width: '100%', height: '50%'}}
                 />
               {/* <img className='smallGraph' src={placeholder} /> */}
               <br />
