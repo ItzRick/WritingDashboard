ced89952689ca91d408da8475d77c5ce7a3d44e1
PFvanBeerendonk <64134830+PFvanBeerendonk@users.noreply.github.com>
2022-05-24 10:02:35 +0200

    added comments

--- WriterDesk1/src/components/UploadSingleFile.js
+++ WriterDesk1/src/components/UploadSingleFile.js
@@ -1,5 +1,5 @@
 
-import { useEffect, useState, useRef, forwardRef, useImperativeHandle  } from 'react';
+import { useState, useRef, forwardRef, useImperativeHandle } from 'react';
 import axios from 'axios';
 
 // materials
@@ -14,57 +14,68 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 /**
  * 
- * @param {} 
+ * @param {*} ref reference to the parent Upload.js
+ * @param {int} thisIndex Index of the UploadSingleFile objects in the list in parent
+ * @param {function} setUploadSingleFiles function in parent to change the list of UploadSingleFile objects
  * @returns Single File Upload Object
  */
 const UploadSingleFile = forwardRef(({ setUploadSingleFiles, thisIndex }, ref) => {
 
     /**
-     * Update uploadSingleFiles in parent Upload.js
+     * Update uploadSingleFiles in parent Upload.js by removing self
      */
     const removeInstance = () => {
         setUploadSingleFiles((list) => list.filter(item => item.props.thisIndex !== thisIndex));
     }
 
     /**
-     * 
+     * File upload function
+     * @param {*} ref reference to parent
      */
     useImperativeHandle(ref, () => ({
         uploadFile() {
-            // url of the file api
+            // url of the file api's upload function
             const url = 'https://localhost:5000/fileapi/upload';
+
+            // create form with all the file information
             const formData = new FormData();
-            formData.append('files',file);
+            formData.append('files', file);
             formData.append('fileName', file.name);
-            formData.append('userId', 123);
+            formData.append('userId', 123);         //TODO change to real uid
             formData.append('date', date.toISOString().substring(0, 10));
             formData.append('courseCode', course);
+            //add header
             const headers = {
                 Accept: 'application/json',
                 'Content-Type': 'multipart/form-data',
             }
+
+            //post the file
             axios.post(url, formData, headers).catch((error) => {
                 console.log(error.response.data);
             });
-                
-          setFile('or drag it here.');
-          setDate(new Date());
-          setCourse('');
-          setDisplayAlertSize(false);
-          setDisplayAlertType(false);
+
+            //post-update
+            //empty file, date and course
+            setFile('or drag it here.');
+            setDate(new Date());
+            setCourse('');
+            //remove warnings
+            setDisplayAlertSize(false);
+            setDisplayAlertType(false);
         },
     }))
 
+    // state of the file drag and drop box. Either 'or drag it here.' or 'FILENAME.TYPE'
     const [file, setFile] = useState('or drag it here.');
-
+    // state of the course box. String
     const [course, setCourse] = useState('');
-
-    const [displayAlertType, setDisplayAlertType] = useState(false);
-
-    const [displayAlertSize, setDisplayAlertSize] = useState(false);
-
+    // state of the course box. String
     const [date, setDate] = useState(new Date());
-    
+
+    // Alerts
+    const [displayAlertType, setDisplayAlertType] = useState(false);
+    const [displayAlertSize, setDisplayAlertSize] = useState(false);
 
     /**
      * Checks if the file is of the correct type, and correct size
@@ -72,34 +83,52 @@ const UploadSingleFile = forwardRef(({ setUploadSingleFiles, thisIndex }, ref) =
      */
     const checkCorrectFile = (file) => {
         const fileType = file.type
+        //file type checkers
         const isPDF = fileType === 'application/pdf';
         const isWord1 = fileType === ('application/vnd.openxmlformats-officedocument.wordprocessingml.document');
         const isWord2 = fileType === 'application/msword';
         const isTXT = fileType === 'text/plain';
+        //boolean for correct type and size
         let isCorrectType = true;
         let isCorrectSize = true;
+
+        //check file type: pdf, doc, docx, txt
         if (!(isPDF || isWord1 || isWord2 || isTXT)) {
+            // incorrect file type
             setDisplayAlertType(true);
             isCorrectType = false;
         } else {
+            // correct file type
             setDisplayAlertType(false);
             isCorrectType = true;
         }
+
+        //check file size: <10MB
         if (file.size > 10485760) {
+            // incorrect file size
             setDisplayAlertSize(true);
             isCorrectSize = false;
         } else {
+            // correct file size
             setDisplayAlertSize(false);
             isCorrectSize = true;
         }
+
+        //change the displayed file
         if (isCorrectSize && isCorrectType) {
+            // all is correct, accept file
             setFile(file);
         } else {
+            // some incorrect, reject file
             setFile('or drag it here.');
         }
     }
 
 
+    /**
+     * Handle file being added to the file-box
+     * @param {FileChangeEvent} event The file in the filebox changed
+     */
     const onFileChange = (event) => {
         if (event.target.files[0] === undefined) {
             // no file here
@@ -115,13 +144,16 @@ const UploadSingleFile = forwardRef(({ setUploadSingleFiles, thisIndex }, ref) =
      * @param {event} event 
      */
     const onFileDrop = (event) => {
+        // make sure the file is not displayed as pdf in the browser
         event.stopPropagation();
         event.preventDefault();
+        //check file correctness
         let dt = event.dataTransfer;
         let files = dt.files;
         checkCorrectFile(files[0])
     }
 
+    // reference so the parent can ask for the file
     const fileInput = useRef();
     return (
         <div style={{ marginBottom: '1vw' }}>
@@ -137,28 +169,28 @@ const UploadSingleFile = forwardRef(({ setUploadSingleFiles, thisIndex }, ref) =
                         style={{ display: 'none' }}
                         accept="application/pdf, application/msword, 
                         application/vnd.openxmlformats-officedocument.wordprocessingml.document, text/plain"
-                        onChange={onFileChange} 
+                        onChange={onFileChange}
                     />
                     {file.name !== undefined ? file.name : 'or drag it here.'}
                 </div>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DatePicker
-                    disableFuture
-                    label="Date"
-                    openTo="day"
-                    views={['year', 'month', 'day']}
-                    value={date}
-                    onChange={(newDate) => {
-                        setDate(newDate);
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
+                        disableFuture
+                        label="Date"
+                        openTo="day"
+                        views={['year', 'month', 'day']}
+                        value={date}
+                        onChange={(newDate) => {
+                            setDate(newDate);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
                     />
                 </LocalizationProvider>
-                <TextField label='course' variant='outlined' value={course} onChange={event => setCourse(event.target.value)}/>
-                <Button variant='contained' sx={{bgcolor: 'red', color: 'button.text'}} value={thisIndex} onClick={removeInstance}>Remove</Button>
+                <TextField label='course' variant='outlined' value={course} onChange={event => setCourse(event.target.value)} />
+                <Button variant='contained' sx={{ bgcolor: 'red', color: 'button.text' }} value={thisIndex} onClick={removeInstance}>Remove</Button>
             </div>
-            {displayAlertType? <Alert severity="error">Upload a file with a .txt, .pdf, .docx or .doc filetype!</Alert> : null}
-            {displayAlertSize? <Alert severity="error">The uploaded file was too big, upload a file that is not larger than 10 MB!</Alert> : null}
+            {displayAlertType ? <Alert severity="error">Upload a file with a .txt, .pdf, .docx or .doc filetype!</Alert> : null}
+            {displayAlertSize ? <Alert severity="error">The uploaded file was too big, upload a file that is not larger than 10 MB!</Alert> : null}
         </div>
     )
 });
