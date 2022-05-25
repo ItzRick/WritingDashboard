2e898e9fc9791e04e800761a43dda4f02b704599
20183163 <b.s.arendsen@student.tue.nl>
2022-05-25 14:20:26 +0200

    fixed multiple pages problem

--- WriterDesk1/src/pages/Document.js
+++ WriterDesk1/src/pages/Document.js
@@ -22,6 +22,12 @@ function Document() {
   const { setTitle } = useOutletContext();
 
   const [file, setFile] = useState('');
+  const [scrollPosY, setScrollPosY] = useState(0);
+  const [mousePosY, setMousePosY] = useState(0);
+
+  const [showTextbox, setShowTextbox] = useState([]);
+
+
 
   useEffect(() => {
     setTitle('Document');
@@ -30,35 +36,55 @@ function Document() {
   const path = 'C:\\Users\\20183163\\PycharmProjects\\SEP2021\\WriterDesk1\\src\\example2.pdf'
   const type = 'pdf'
 
-  const handleScroll = (e) => {
-
-    console.log("scrollTop", e.target.scrollTop);
-    console.log("scrollHeight", e.target.scrollHeight);
-    console.log("scrollWidth", e.target.scrollWidth);
-  }
-
   const mistakes = [
-  {name: 1, explanation: 'expl1', color: 'rgba(200, 0, 0, 0.3)', coords: [156.9016876220703, 157.89927673339844, 183.29876708984375, 169.56455993652344]},
-  {name: 2, explanation: 'expl2', color: 'rgba(0, 200, 0, 0.3)', coords: [464.495361328125, 468.1363525390625, 514.29833984375, 480.14129638671875]},
-  {name: 3, explanation: 'expl3', color: 'rgba(0, 0, 200, 0.3)', coords: [126.9016876220703, 157.89927673339844, 213.29876708984375, 169.56455993652344]}
+  {text: 'decade', explanation: 'expl1', type: 0, coords: [156.9016876220703, 157.89927673339844, 183.29876708984375, 169.56455993652344], page: 0},
+  {text: 'Furthermore', explanation: 'expl2', type: 1, coords: [464.495361328125, 468.1363525390625, 514.29833984375, 480.14129638671875], page: 0},
+  {text: 'past decade, a new', explanation: 'expl3', type: 2, coords: [126.9016876220703, 157.89927673339844, 213.29876708984375, 169.56455993652344], page: 0},
+  {text: 'semantics', explanation: 'expl4', type: 3, coords: [390.88116455078125, 66.0565185546875, 430.1736755371094, 78.06145477294922], page: 1}
   ];
 
+
+  const handleScroll = (e) => {
+    setScrollPosY(e.target.scrollTop)
+  }
 
   const handleClick = (e) => {
     let rect = e.target.getBoundingClientRect();
     let x = e.clientX - rect.left;
     let y = e.clientY - rect.top;
-
+    let pageClick = getPage(scrollPosY + mousePosY - 192 - y, rect.height);
+    let newArrShowTextbox = [...showTextbox];
 
     for (let i = 0; i < mistakes.length; i++) {
-      if ((mistakes[i].coords[0] <= x) && (x <= mistakes[i].coords[2]) && (mistakes[i].coords[1] <= y) && (y <= mistakes[i].coords[3])) {
-        console.log(x, y);
-        document.getElementById('textBoxExpl' + i).style.display = "block";
-      } else {
-        document.getElementById('textBoxExpl' + i).style.display = "none";
-      }
+      let left = mistakes[i].coords[0];
+      let right = mistakes[i].coords[2];
+      let top = mistakes[i].coords[1];
+      let bottom = mistakes[i].coords[3];
+      let mistakePage = mistakes[i].page;
+      newArrShowTextbox[i] = (left <= x) && (x <= right) && (top <= y) && (y <= bottom) && (pageClick === mistakePage);
     }
+    setShowTextbox(newArrShowTextbox);
+  }
 
+  function getPage(pdfPos, pageHeight) {
+    // find the quotient
+    let q = parseInt(pdfPos / pageHeight);
+
+    // 1st possible closest number
+    let n1 = pageHeight * q;
+
+    // 2nd possible closest number
+    let n2 = (pdfPos * pageHeight) > 0 ?
+        (pageHeight * (q + 1)) : (pageHeight * (q - 1));
+
+    // if true, then n1 is the
+    // required closest number
+    if (Math.abs(pdfPos - n1) < Math.abs(pdfPos - n2))
+        return parseInt(n1/pageHeight);
+
+    // else n2 is the required
+    // closest number
+    return parseInt(n2/pageHeight);
   }
 
   const mousePointer = (e) => {
@@ -66,28 +92,62 @@ function Document() {
     let x = e.clientX - rect.left;
     let y = e.clientY - rect.top;
     let setPointer = false;
+    setMousePosY(e.screenY);
 
-
-    for (let i = 0; i < mistakes.length; i++) {
-      if (((mistakes[i].coords[0] <= x) && (x <= mistakes[i].coords[2]) && (mistakes[i].coords[1] <= y) && (y <= mistakes[i].coords[3]))) {
-        setPointer = true
-      }
-    }
-
-    if (setPointer) {
-        document.getElementById('all-page-container').style.cursor = "pointer";
-    } else {
-        document.getElementById('all-page-container').style.cursor = "default";
-    }
+    // let page = getPage(scrollPosY + mousePosY - 192 - y, rect.height);
+    //
+    // for (let i = 0; i < mistakes.length; i++) {
+    //   if (((mistakes[i].coords[0] <= x) && (x <= mistakes[i].coords[2]) && (mistakes[i].coords[1] <= y) && (y <= mistakes[i].coords[3])) && page === mistakes[i].page) {
+    //     setPointer = true
+    //   }
+    // }
+    //
+    // if (setPointer) {
+    //     document.getElementById('all-page-container').style.cursor = "pointer";
+    // } else {
+    //     document.getElementById('all-page-container').style.cursor = "default";
+    // }
 
   }
 
-  const TextBoxExplanation = props => (
-      <div className='textBoxExpl' id={'textBoxExpl' + props.number} style={{backgroundColor: props.color, borderColor:props.color}}>
-        <Typography>{props.mistake}</Typography>
-        <Typography>{props.expl}</Typography>
-      </div>
-  )
+
+  const TextBoxExplanation = (props) => {
+    return(
+    <div className={showTextbox[props.number] ? 'textBoxExpl' : 'hidden'} id={'textBoxExpl' + props.number}
+         style={{backgroundColor: typeToColor(props.type), borderColor: typeToColor(props.type)}}>
+      <Typography variant='body1'><b>{props.text}</b></Typography>
+      <Typography variant='body1' style={{color: typeToColor(props.type), textAlign: 'right'}}><b>typeToName(props.type)</b></Typography>
+      <Typography variant='body2'>{props.expl}</Typography>
+    </div>
+    );
+  };
+
+  const typeToColor = (type) => {
+    if (type === 0) {
+      return 'rgba(100, 143, 255, 0.5)'
+    } else if (type === 1) {
+      return 'rgba(220, 38, 127, 0.5)'
+    } else if (type === 2) {
+      return 'rgba(254, 97, 0, 0.5)'
+    } else {
+      return 'rgba(255, 176, 0, 0.5)'
+    }
+
+  };
+
+  const typeToName = (type) => {
+    if (type === 0) {
+      return 'Language and Style'
+    } else if (type === 1) {
+      return 'Cohesion'
+    } else if (type === 2) {
+      return 'Structure'
+    } else {
+      return 'Source Integration and Content'
+    }
+
+  };
+
 
 
   return (
@@ -100,7 +160,7 @@ function Document() {
         <br />
 
         {/*<TextBoxExplanation mistake={'mistake'} color = {'rgba(200, 0, 0, 0.3)'}/>*/}
-        {mistakes.map((mistake, i) => <TextBoxExplanation key={i} number={i} mistake={mistake.name} color={mistake.color} expl={mistake.explanation}/>)}
+        {mistakes.map((mistake, i) => <TextBoxExplanation key={i} number={i} text={mistake.text} type={mistake.type} expl={mistake.explanation}/>)}
       </div>
     </>
 
