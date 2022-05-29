82932c910f31bbb0ad3362185868bb73c85f4e29
20183163 <b.s.arendsen@student.tue.nl>
2022-05-29 17:50:49 +0200

    explanation div design

--- WriterDesk1/src/pages/Document.js
+++ WriterDesk1/src/pages/Document.js
@@ -37,7 +37,7 @@ function Document() {
   const type = 'pdf'
 
   const mistakes = [
-  {text: 'decade', explanation: 'expl1', type: 0, coords: [156.9016876220703, 157.89927673339844, 183.29876708984375, 169.56455993652344], page: 0, replacements: ['a', 'b', 'c']},
+  {text: 'decade', explanation: 'expl1', type: 0, coords: [156.9016876220703, 157.89927673339844, 183.29876708984375, 169.56455993652344], page: 0, replacements: ['ab', 'b', 'ba']},
   {text: 'Furthermore', explanation: 'expl2', type: 1, coords: [464.495361328125, 468.1363525390625, 514.29833984375, 480.14129638671875], page: 0, replacements: ['as']},
   {text: 'past decade, a new ', explanation: 'expl3', type: 2, coords: [126.9016876220703, 157.89927673339844, 213.29876708984375, 169.56455993652344], page: 0, replacements: ['a', 'b', 'c']},
   {text: 'semantics', explanation: 'expl4', type: 3, coords: [390.88116455078125, 66.0565185546875, 430.1736755371094, 78.06145477294922], page: 1, replacements: []}
@@ -68,13 +68,25 @@ function Document() {
     return(
     <div className={showTextbox[props.number] ? 'textBoxExpl' : 'hidden'} id={'textBoxExpl' + props.number}
          style={{backgroundColor: typeToColor(props.type), borderColor: typeToColor(props.type)}}>
-      <Typography className='textBoxType' style={{color: typeToColor(props.type)}}><b>{typeToName(props.type)}</b></Typography>
+      <Typography className='textBoxType' style={{color: typeToColor(props.type)}}>
+        <b>{typeToName(props.type)}</b>
+      </Typography>
       <Typography variant='body1' className='textBoxWord'><b>{props.text}</b></Typography>
-      <Typography variant='body2' style={{clear: 'both', marginTop: '30px', marginBottom:'10px'}}>{props.expl}</Typography>
-      <Typography className={props.replacements.length > 0 ? 'replacementsText' : 'hidden'} variant='body2'> Possible replacements:</Typography>
-      <Typography className='textBoxReplacements' variant='body1' sx={{ marginLeft: 0.6, marginRight: 0.6 }} style={{backgroundColor: typeToColor(props.type)}}> {props.replacements[0]}</Typography>
-      <Typography className='textBoxReplacements' variant='body1' sx={{ marginLeft: 0.6, marginRight: 0.6 }} style={{backgroundColor: typeToColor(props.type)}}> {props.replacements[1]}</Typography>
-      <Typography className='textBoxReplacements' variant='body1' sx={{ marginLeft: 0.6, marginRight: 0.6 }} style={{backgroundColor: typeToColor(props.type)}}> {props.replacements[2]}</Typography>
+      <Typography variant='body2' style={{clear: 'both', marginTop: '30px', marginBottom:'10px'}}>
+        {props.expl}
+      </Typography>
+      <Typography className={props.replacements.length > 0 ? 'replacementsText' : 'hidden'} variant='body2'>
+        Possible replacements:
+      </Typography>
+      <Typography className='textBoxReplacements' variant='body1' sx={{ marginLeft: 0.6, marginRight: 0.6 }}
+                  style={{backgroundColor: typeToColor(props.type)}}> {props.replacements[0]}
+      </Typography>
+      <Typography className='textBoxReplacements' variant='body1' sx={{ marginLeft: 0.6, marginRight: 0.6 }}
+                  style={{backgroundColor: typeToColor(props.type)}}> {props.replacements[1]}
+      </Typography>
+      <Typography className='textBoxReplacements' variant='body1' sx={{ marginLeft: 0.6, marginRight: 0.6 }}
+                  style={{backgroundColor: typeToColor(props.type)}}> {props.replacements[2]}
+      </Typography>
     </div>
     );
   };
@@ -112,7 +124,6 @@ function Document() {
     } else {
       return 'Source Integration and Content'
     }
-
   };
 
 
