f90f258c903ad4b93b4dda69f528051b2c81b412
20183163 <b.s.arendsen@student.tue.nl>
2022-05-30 16:31:27 +0200

    Comments

--- WriterDesk1/src/pages/Document.js
+++ WriterDesk1/src/pages/Document.js
@@ -34,20 +34,25 @@ function Document() {
   const type = 'pdf'
 
   const mistakes = [
-  {text: 'decade', explanation: 'expl1', type: 0, coords: [156.9016876220703, 157.89927673339844, 183.29876708984375, 169.56455993652344], page: 0, pageHeight: 0, replacements: ['ab', 'b', 'ba']},
-  {text: 'Furthermore', explanation: 'expl2', type: 1, coords: [464.495361328125, 468.1363525390625, 514.29833984375, 480.14129638671875], page: 0, pageHeight: 0, replacements: ['as']},
-  {text: 'past decade, a new ', explanation: 'expl3', type: 2, coords: [126.9016876220703, 157.89927673339844, 213.29876708984375, 169.56455993652344], page: 0, pageHeight: 0, replacements: ['a', 'b', 'c']},
-  {text: 'semantics', explanation: 'expl4', type: 3, coords: [390.88116455078125, 66.0565185546875, 430.1736755371094, 78.06145477294922], page: 1, pageHeight: 792, replacements: []}
+    {text: 'decade', explanation: 'expl1', type: 0, coords: [156.9016876220703, 157.89927673339844, 183.29876708984375, 169.56455993652344], page: 0, pageHeight: 0, replacements: ['ab', 'b', 'ba']},
+    {text: 'Furthermore', explanation: 'expl2', type: 1, coords: [464.495361328125, 468.1363525390625, 514.29833984375, 480.14129638671875], page: 0, pageHeight: 0, replacements: ['as']},
+    {text: 'past decade, a new ', explanation: 'expl3', type: 2, coords: [126.9016876220703, 157.89927673339844, 213.29876708984375, 169.56455993652344], page: 0, pageHeight: 0, replacements: ['a', 'b', 'c']},
+    {text: 'semantics', explanation: 'expl4', type: 3, coords: [390.88116455078125, 66.0565185546875, 430.1736755371094, 78.06145477294922], page: 1, pageHeight: 792, replacements: []}
   ];
 
 
-
-  const highlightClick = (e, coords, pageNr) => {
+  /**
+   * Function to show the explanation boxes that are being clicked
+   * @param {Object} e - Click event
+   * @param {Object} coords - Array of the coordinates of the mistake being clicked
+   * @param {number} clickPageNr - Number of the page where is clicked
+   */
+  const handleHighlightClick = (e, coords, clickPageNr) => {
     let rect = e.target.getBoundingClientRect();
-    let x = coords[0] + e.clientX - rect.left;
-    let y = coords[1] + e.clientY - rect.top;
+    let x = coords[0] + e.clientX - rect.left; // x-coordinate of click in div
+    let y = coords[1] + e.clientY - rect.top; // y-coordinate of click in div
 
-    let newArrShowTextbox = [...showTextbox];
+    let newArrShowTextbox = []; // Create new array to overwrite showTextbox
 
     for (let i = 0; i < mistakes.length; i++) {
       let left = mistakes[i].coords[0];
@@ -55,43 +60,60 @@ function Document() {
       let top = mistakes[i].coords[1];
       let bottom = mistakes[i].coords[3];
       let mistakePage = mistakes[i].page;
-      newArrShowTextbox[i] = (left <= x) && (x <= right) && (top <= y) && (y <= bottom) && (pageNr === mistakePage);
+
+      //Set showTextbox true for every mistake that is clicked
+      newArrShowTextbox[i] = (left <= x) && (x <= right) && (top <= y) && (y <= bottom) && (clickPageNr === mistakePage);
     }
-    setShowTextbox(newArrShowTextbox);
+    setShowTextbox(newArrShowTextbox); // Overwrite showTextbox
   }
 
 
-
+  /**
+   * Div that shows the explanation of a mistake being clicked
+   * @property {number} number - Number of mistake
+   * @property {number} type - Type of mistake in number
+   * @property {String} expl - Explanation of the mistake
+   * @property {Object} replacements - Array of max 3 replacements for the mistake
+   * @returns TextBoxExplanation component
+   */
   const TextBoxExplanation = (props) => {
     return(
-    <div className={showTextbox[props.number] ? 'textBoxExpl' : 'hidden'} id={'textBoxExpl' + props.number}
-         style={{backgroundColor: typeToColor(props.type), borderColor: typeToColor(props.type)}}>
-      <Typography className='textBoxType' style={{color: typeToColor(props.type)}}>
-        <b>{typeToName(props.type)}</b>
-      </Typography>
-      <Typography variant='body1' className='textBoxWord'><b>{props.text}</b></Typography>
-      <Typography variant='body2' style={{clear: 'both', marginTop: '30px', marginBottom:'10px'}}>
-        {props.expl}
-      </Typography>
-      <Typography className={props.replacements.length > 0 ? 'replacementsText' : 'hidden'} variant='body2'>
-        Possible replacements:
-      </Typography>
-      <Typography className='textBoxReplacements' variant='body1' sx={{ marginLeft: 0.6, marginRight: 0.6 }}
-                  style={{backgroundColor: typeToColor(props.type)}}> {props.replacements[0]}
-      </Typography>
-      <Typography className='textBoxReplacements' variant='body1' sx={{ marginLeft: 0.6, marginRight: 0.6 }}
-                  style={{backgroundColor: typeToColor(props.type)}}> {props.replacements[1]}
-      </Typography>
-      <Typography className='textBoxReplacements' variant='body1' sx={{ marginLeft: 0.6, marginRight: 0.6 }}
-                  style={{backgroundColor: typeToColor(props.type)}}> {props.replacements[2]}
-      </Typography>
-    </div>
+      <div className={showTextbox[props.number] ? 'textBoxExpl' : 'hidden'} id={'textBoxExpl' + props.number}
+           style={{backgroundColor: typeToColor(props.type), borderColor: typeToColor(props.type)}}>
+        <Typography className='textBoxType' style={{color: typeToColor(props.type)}}>
+          <b>{typeToName(props.type)}</b>
+        </Typography>
+        <Typography variant='body1' className='textBoxWord'><b>{props.text}</b></Typography>
+        <Typography variant='body2' sx={{marginTop: '5px', marginBottom:'10px'}}>
+          {props.expl}
+        </Typography>
+        <Typography className={props.replacements.length > 0 ? 'replacementsText' : 'hidden'} variant='body2'>
+          Possible replacements:
+        </Typography>
+        <Typography className='textBoxReplacements' variant='body1' sx={{ marginLeft: 0.6, marginRight: 0.6 }}
+                    style={{backgroundColor: typeToColor(props.type)}}> {props.replacements[0]}
+        </Typography>
+        <Typography className='textBoxReplacements' variant='body1' sx={{ marginLeft: 0.6, marginRight: 0.6 }}
+                    style={{backgroundColor: typeToColor(props.type)}}> {props.replacements[1]}
+        </Typography>
+        <Typography className='textBoxReplacements' variant='body1' sx={{ marginLeft: 0.6, marginRight: 0.6 }}
+                    style={{backgroundColor: typeToColor(props.type)}}> {props.replacements[2]}
+        </Typography>
+      </div>
     );
   };
 
+
+  /**
+   * Transparent div that handles onClick events on highlighted text
+   * @property {Object} coords - Array of the coordinates on the page where the mistake is
+   * @property {number} page - Page number where the mistake is
+   * @property {number} pageHeight - Coordinates of top of page in file
+   * @returns ClickableTextDiv component
+   */
   const ClickableTextDiv = (props) => {
     return(
-     <div onClick={e=>highlightClick(e, props.coords, props.page)} className='clickableTextDiv'
+     <div onClick={e=>handleHighlightClick(e, props.coords, props.page)} className='clickableTextDiv'
           style={{left: props.coords[0], top: props.coords[1] + props.pageHeight,
             width: props.coords[2] - props.coords[0],
             height: props.coords[3] - props.coords[1]}}>
@@ -99,18 +121,30 @@ function Document() {
     );
   };
 
+
+  /**
+   * Function to get the correct color for a mistake type
+   * @param {number} type - Number for type of mistake
+   * @returns {String} Color matched to the input type
+   */
   const typeToColor = (type) => {
-    if (type === 0) {
-      return 'rgba(100, 143, 255, 0.5)'
-    } else if (type === 1) {
-      return 'rgba(220, 38, 127, 0.5)'
-    } else if (type === 2) {
-      return 'rgba(254, 97, 0, 0.5)'
-    } else {
-      return 'rgba(255, 176, 0, 0.5)'
+    if (type === 0) { // Language and Style
+      return '#648FFF80'
+    } else if (type === 1) { // Cohesion
+      return '#FE610080'
+    } else if (type === 2) { // Structure
+      return '#FFB00080'
+    } else { // Source Integration and Content
+      return '#DC267F80'
     }
   };
 
+
+  /**
+   * Function to get the correct name for a mistake type
+   * @param {number} type - Number for type of mistake
+   * @returns {String} Name matched to the input type
+   */
   const typeToName = (type) => {
     if (type === 0) {
       return 'Language and Style'
@@ -122,7 +156,6 @@ function Document() {
       return 'Source Integration and Content'
     }
   };
-
 
 
   return (
@@ -145,7 +178,6 @@ function Document() {
         )}
       </div>
     </>
-
   );
 }
 
