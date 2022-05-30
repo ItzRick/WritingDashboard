8c8e2f62ae5c4f032626ab67653ad022abc5cba5
PFvanBeerendonk <64134830+PFvanBeerendonk@users.noreply.github.com>
2022-05-30 17:15:27 +0200

    added logo to Landing

--- WriterDesk1/src/pages/LandingPage.js
+++ WriterDesk1/src/pages/LandingPage.js
@@ -1,10 +1,12 @@
 import './../css/LoginSignUp.css';
 
 // materials
-import { 
-  Button, 
-  Typography 
+import {
+  Button,
+  Typography,
+  Box,
 } from "@mui/material";
+import logo from '../images/logo.png'
 
 // routing
 import { Link } from 'react-router-dom';
@@ -14,29 +16,39 @@ import { Link } from 'react-router-dom';
  * @returns landing page, main page when not logged in
  */
 const LandingPage = () => {
-  
+
   return (
-    <>
-      <Typography variant='h2'>Writing Dashboard</Typography>
+    <Box classname='center'>
+      <Box className='center'>
+        <Box sx={{
+          display: 'inline-flex',
+        }}>
+          <Typography className='titleChild' variant='h2'> Writing </Typography>
+          <img className='logo titleChild' src={logo} />
+          <Typography variant='h2' className='titleChild'> Dashboard </Typography>
+        </Box>
+      </Box>
       <br />
       <Typography variant='h5'>Improve your academic writing.</Typography>
       <br />
       <div className='center'>
-          <Button size='large' sx={{bgcolor: 'button.main', color: 'button.text'}} variant='contained' component={Link}  to={'/Login'}>Log in</Button>
-          <div className='filler'></div>
-          <Button size='large' sx={{bgcolor: 'button.main', color: 'button.text'}} variant='contained' component={Link}  to={'/SignUp'}>Sign up</Button>
+        <Button size='large' sx={{ bgcolor: 'button.main', color: 'button.text' }} variant='contained' component={Link} to={'/Login'}>Log in</Button>
+        <div className='filler'></div>
+        <Button size='large' sx={{ bgcolor: 'button.main', color: 'button.text' }} variant='contained' component={Link} to={'/SignUp'}>Sign up</Button>
       </div>
       <br /><br />
       <div className='center'>
-          <span className='textbox'>In this application, TU/e students can improve their academic writing.
-              This is done by uploading documents which have been written during their studies.
-              After the document has been uploaded, feedback will be generated.
-              The student can view the feedback, which includes the feedback scores.
-              After uploading multiple documents, the students can view their progress over time.
-              Lorem ipsum, lorem ipsum. </span>
+        <span className='textbox'>
+          In this application, TU/e students can improve their academic writing.
+          This is done by uploading documents which have been written during their studies.
+          After the document has been uploaded, feedback will be generated.
+          The student can view the feedback, which includes the feedback scores.
+          After uploading multiple documents, the students can view their progress over time.
+          Lorem ipsum, lorem ipsum.
+        </span>
       </div>
-    </>
+    </Box>
   );
- }
+}
 
- export default LandingPage;
+export default LandingPage;
--- WriterDesk1/src/pages/Login.js
+++ WriterDesk1/src/pages/Login.js
@@ -7,7 +7,7 @@ import {
     Typography,
     IconButton 
 } from "@mui/material";
-import testImage from '../images/placeholder_image.png'
+import logo from '../images/logo.png';
 
 // routing
 import { Link, useOutletContext } from 'react-router-dom';
@@ -29,7 +29,7 @@ const Login = () => {
             <div className='parent'>
                 <div className='div1'>
                     <IconButton style={{ float: 'left' }} component={Link} to='/LandingPage'>
-                        <img className='logo' src={testImage} />
+                        <img className='logo' src={logo} />
                     </IconButton>
                 </div>
                 <div className='div2'>
