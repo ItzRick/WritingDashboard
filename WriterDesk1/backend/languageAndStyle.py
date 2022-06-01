b2f1a1b2978592bf7768d9fba514f9b5b6fae87c
20183163 <b.s.arendsen@student.tue.nl>
2022-06-01 14:49:54 +0200

    Added occurrence count in feedbackLanguageStyle function

--- WriterDesk1/backend/languageAndStyle.py
+++ WriterDesk1/backend/languageAndStyle.py
@@ -1,4 +1,5 @@
 import language_tool_python
+import re
 
 
 def feedbackLanguageStyle(text):
@@ -8,9 +9,14 @@ def feedbackLanguageStyle(text):
         tool: LanguageTool object to generate feedback.
         matches: List of Match objects containing the feedback.
         match: Match object containing the feedback.
+        prevMatchEnd: End character of previous match in loop.
+        occurrenceCount: count the occurrences of a mistake in the context.
+        occurrenceInContext: The number of the correct occurrence of the mistake in the context.
+        wordMatch: Match of word in context.
+        context: context of mistake text.
     :param text: Input string that will be given feedback on.
     :returns:
-        mistakes: List of mistakes in text including matched text, sentence, explanation, and possible replacements.
+        mistakes: List of mistakes in text including matched text, context, occurrence of text in context, explanation, and possible replacements.
         score: Score between 0 and 10 given to the text based on the feedback.
     """
 
@@ -22,8 +28,32 @@ def feedbackLanguageStyle(text):
 
     mistakes = []
     for match in matches:
-        # Append matched text, sentence, explanation, and top 3 replacements to a list
-        mistakes.append([match.matchedText, match.sentence, match.message, match.replacements[:3]])
+
+        prevMatchEnd = -1
+        occurrenceCount = 0
+        occurrenceInContext = 0
+
+        # Find all occurrences of a mistake in the context
+        for wordMatch in re.finditer(match.matchedText.lower(), match.context.lower()):
+            if not (wordMatch.start() <= prevMatchEnd):
+                # Mistake overlaps previous mistake and does not count to occurrences
+
+                if wordMatch.start() == match.offsetInContext:
+                    occurrenceInContext = occurrenceCount  # Set occurrenceInContext
+                else:
+                    occurrenceCount += 1
+            prevMatchEnd = wordMatch.end()
+
+        context = match.context
+
+        # Remove starting and ending ...
+        if context[:3] == '...':
+            context = context[3:]
+        if context[-3:] == '...':
+            context = context[:-3]
+
+        # Append matched text, context, occurrence in context, explanation, and top 3 replacements to a list
+        mistakes.append([match.matchedText, context, occurrenceInContext, match.message, match.replacements[:3]])
 
     # Compute score
     score = calculateScore(len(matches), len(text.split()))
