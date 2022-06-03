2e723a0aca925364a59deb2542aeaf17b81561dc
20183163 <b.s.arendsen@student.tue.nl>
2022-06-03 10:49:47 +0200

    Added comments to testcases

--- WriterDesk1/backend/tests/functional/test_convertToText.py
+++ WriterDesk1/backend/tests/functional/test_convertToText.py
@@ -3,64 +3,112 @@ import os
 
 
 def testGetDocxHeading():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_path)
+    """
+        Test if the getDOCXText function retrieves the text from the docx file without headings.
+        Attributes:
+            dir_path: Path of the location where the file is stored.
+            text: String that contains the output of the getDOCXText function.
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))  # Get directory path
+    os.chdir(dir_path)  # Set directory path
 
     text = getDOCXText('headingTest.docx')
     assert text == 'This is some text.\n\nMore text.'
 
 
 def testGetDocxReferences():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_path)
+    """
+        Test if the getDOCXText function retrieves the text from the docx file without references.
+        Attributes:
+            dir_path: Path of the location where the file is stored.
+            text: String that contains the output of the getDOCXText function.
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))  # Get directory path
+    os.chdir(dir_path)  # Set directory path
 
     text = getDOCXText('referencesTest.docx')
     assert text == 'Text.\n\nMore text.\n\nNew text.'
 
 
 def testGetDocxImages():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_path)
+    """
+        Test if the getDOCXText function retrieves the text from the docx file without images and corresponding captions.
+        Attributes:
+            dir_path: Path of the location where the file is stored.
+            text: String that contains the output of the getDOCXText function.
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))  # Get directory path
+    os.chdir(dir_path)  # Set directory path
 
     text = getDOCXText('imagesTest.docx')
     assert text == 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.'
 
 
 def testGetDocxTextboxes():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_path)
+    """
+        Test if the getDOCXText function retrieves the text from the docx file without text from textboxes.
+        Attributes:
+            dir_path: Path of the location where the file is stored.
+            text: String that contains the output of the getDOCXText function.
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))  # Get directory path
+    os.chdir(dir_path)  # Set directory path
 
     text = getDOCXText('textboxTest.docx')
     assert text == 'This is text outside a textbox.'
 
 
 def testGetDocxEmptyFile():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_path)
+    """
+        Test if the getDOCXText function outputs an empty string when using an empty file.
+        Attributes:
+            dir_path: Path of the location where the file is stored.
+            text: String that contains the output of the getDOCXText function.
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))  # Get directory path
+    os.chdir(dir_path)  # Set directory path
 
     text = getDOCXText('emptyFile.docx')
     assert text == ''
 
 
 def testGetDocxCorruptedFile():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_path)
+    """
+        Test if the getDOCXText function outputs an empty string when using a corrupted file.
+        Attributes:
+            dir_path: Path of the location where the file is stored.
+            text: String that contains the output of the getDOCXText function.
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))  # Get directory path
+    os.chdir(dir_path)  # Set directory path
 
     text = getDOCXText('corruptedFile.docx')
     assert text == ''
 
 
 def testGetDocxInvalidFile():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_path)
+    """
+        Test if the getDOCXText function outputs an empty string when using a file path that does not exist.
+        Attributes:
+            dir_path: Path of the location where the file is stored.
+            text: String that contains the output of the getDOCXText function.
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))  # Get directory path
+    os.chdir(dir_path)  # Set directory path
 
     text = getDOCXText('invalidFileName.docx')
     assert text == ''
 
 
 def testGetDocxInvalidExtension():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_path)
+    """
+        Test if the getDOCXText function outputs an empty string when using a file that is not a docx file.
+        Attributes:
+            dir_path: Path of the location where the file is stored.
+            text: String that contains the output of the getDOCXText function.
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))  # Get directory path
+    os.chdir(dir_path)  # Set directory path
 
     text = getDOCXText('invalidFileExtension.pdf')
     assert text == ''
