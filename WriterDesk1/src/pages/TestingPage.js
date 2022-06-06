8511c3df5c4985014cb7cdaa1cb6f1b0447ae1ce
marklangedijk <m.l.langedijk@student.tue.nl>
2022-06-06 22:55:40 +0200

    Testing Page; Ex 1

--- WriterDesk1/src/pages/TestingPage.js
+++ WriterDesk1/src/pages/TestingPage.js
@@ -1,7 +1,9 @@
 import {
-    
+    Button
 } from "@mui/material";
 import TestingComponent from "./../components/TestingComponent";
+import { AuthenticationService } from "../services/authenticationService";
+import { useState } from "react";
 
 /**
  * page used for testing, to be removed later
@@ -9,12 +11,38 @@ import TestingComponent from "./../components/TestingComponent";
  * @returns Testing Page
  */
 const TestingPage = () => {
-    
+    const [role, setRole] = new useState('')
+
+    const handleLogin = () => {
+        AuthenticationService.login('m.l.langedijk@student.tue.nl', 'wachtwoord');
+    }
+
+    const handleFLogin = () => {
+        AuthenticationService.login('m.l.langedijk@student.tue.nl', 'Fwachtwoord');
+    }
+
+    const handleRole = () => {
+        AuthenticationService.getRole()
+        .then(role => {
+            setRole(role);
+        }).catch(error =>{
+            setRole('');
+        });
+        //setRole(JSON.parse(localStorage.getItem('currentUser')).role);
+    }
     return (
-        {true ? <>
-            Testing
-        </> : <></>}
-    );
+    <>
+    {role != '' &&
+        <p>Role: {role}</p>
+    }
+    <Button variant="contained" sx={{bgcolor: 'button.main', color: 'button.text'}} onClick={handleLogin}>Login</Button>
+    <br/>
+    <Button variant="contained" sx={{bgcolor: 'button.main', color: 'button.text'}} onClick={handleFLogin}>False Login</Button>
+    <br/>
+    <Button variant="contained" sx={{bgcolor: 'button.main', color: 'button.text'}} onClick={handleRole}>Get Role</Button>
+        {role != '' ? <> {role} </> : <> no role </>}
+    </>
+    )
 }
 
 
