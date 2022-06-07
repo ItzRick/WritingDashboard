2348ebc52fec518de9fe30d7ce1d82a6e07d00dc
PFvanBeerendonk <64134830+PFvanBeerendonk@users.noreply.github.com>
2022-06-07 16:28:24 +0200

    initial setup of page

---
+++ WriterDesk1/src/pages/Projects.js
@@ -0,0 +1,22 @@
+import {
+    
+} from "@mui/material";
+
+/**
+ * TODO
+ * 
+ * @returns Projects Page
+ */
+const Projects = () => {
+    
+    return (
+        <>
+            Projects
+        </>
+    );
+}
+
+
+
+
+export default Projects;
