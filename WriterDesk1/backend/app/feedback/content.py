7388b5373c30f19d402e1247bf280a1f0e38a71b
RIck Overmeer <r.j.overmeer@student.tue.nl>
2022-06-07 12:43:57 +0200

    Methods to count the number of words in the text and the words in the sources.

--- WriterDesk1/backend/app/feedback/content.py
+++ WriterDesk1/backend/app/feedback/content.py
@@ -3,7 +3,6 @@ import nltk
 # nltk.download('stopwords')
 # nltk.download('punkt')
 from nltk.corpus import stopwords
-stopwords.words('english')
 from nltk.tokenize import word_tokenize
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
@@ -15,24 +14,31 @@ import os
 
 
 
-def sourceIntegration(text, input_words):
+def sourceIntegration(text, input_words, englishStopwords):
     counts = {}
     for word in input_words:
         counts[word] = (len(re.findall(word, text)))
     return counts
 
 
-def findWords(text, tokens_wo_stopwords):
+def wordsSource(text, wordsWoStopwords, englishStopwords):
     text = re.sub('[,\.!?]', '', text)
-    english_stopwords = stopwords.words('english')
-    
-    # text = text.lower()
     tokens = word_tokenize(text.lower())
     for t in tokens:
-        if t not in english_stopwords:
-            tokens_wo_stopwords.add(t)
+        if t not in englishStopwords:
+            wordsWoStopwords.add(t)
+    return wordsWoStopwords
 
-    return tokens_wo_stopwords
+def countTotalWordsText(text, englishStopwords):
+    count = 0
+    text = re.sub('[,\.!?]', '', text)
+    
+    tokens = word_tokenize(text.lower())
+    for t in tokens:
+        if t not in englishStopwords:
+            count += 1
+
+    return count
 
 def downloadPapers(doi):
     userId = 123
@@ -142,6 +148,16 @@ def scrape_page(url):
 
     return output
 
+string = """The last decade has seen a wide spread of self-tracking technology, which allows people to collect and monitor data about themselves. This rise was enabled by technological advancements such as smaller sensors that are better and more often integrated into mobile devices, the ubiquity of access to information through the internet, and improvements in visualization techniques. As a result, self-tracking technology and systems that help people collect and reflect on their data are widely commercially available, particularly related to health and wellbeing. Examples are systems that help people keep track of sleep (Lullaby), physical fitness (Nike+, Fitbit, UbiFit), their food intake or diet (Loseit, Calorie Counter), and mood (Expereal, T2 Mood Tracker). 
+The most dedicated and well-organized community using these types of technology might be the Quantified-Self (QS) movement, seeking “self-knowledge through numbers”. This community of self-trackers and life-loggers experiments to find the best ways to collect data, and therefore insights, about themselves, and organizes 'meetups' to share knowledge and experiences. However, much more people keep track of some information about themselves than these ‘hard-core’ QS-users. In 2013, Fox and Duggan estimated that 69% of Americans keeps track of at least one health-related parameter for themselves or a loved one, and while no more recent numbers were found, we can imagine the rise in self-tracking technology has only increased this percentage. 
+All of these data, however, only become useful when it leads to better understanding, increased awareness, or the ability to make better decisions based on it. This is what the scientific field of Personal Informatics (PI) focuses on: how can technology facilitate users in the collection of personal data, and more importantly, reflection on this data, to promote behavior change towards healthier habits? In general, the PI and QS movements, therefore, have the same underlying motivation: knowing one’s data, one can learn from it, and use that new knowledge to make informed changes to one’s behavior – and eventually to reach health-related goals. Self-tracking Technology can help people to do this by enabling easy data collection, and by enabling the user to reflect on the data. For example, an activity tracker can help users to gain insight into when and how much they are active ("I'm never active on weekdays"). These insights can enable users to identify goals towards a healthier lifestyle ("I should be more active during workdays"), and find out how to change their behavior to reach these goals ("If I take a walk during my breaks, I will be active enough"). 
+However, reflection that leads to behavior change is a time-consuming and effortful process that does not happen automatically. While people using self-tracking devices often gain low-level insights, such as, when reflecting on their data, this only rarely results in the formulation of high-level health-related goals, or ways to reach these goals. This shows a gap between on the one hand the ability to collect and visualize data and on the other the ability to act upon these data and learn from it. As the main goal of self-trackers is to use their data to achieve informed behavior change, it is problematic that many users do not seem to be able to utilize their data in this way. 
+To support users in making this step from low-level insights to high-level resolutions or plans of action, the aforementioned gap needs to be bridged – by facilitating reflection. The two most influential models of Personal Informatics, lived-informatics model of PI) both name ‘Reflection’ as the step users need to make to move from examination of data to data-driven action. Additionally, Reflection is a key element of successful behavior change. Still, while different studies stress the importance of reflection and supporting it, they are not concrete about how this should be done. 
+An important first step in which reflection on data can be supported is by visualizing the self-tracked data, for example on a dashboard. Data dashboards have the potential to enhance higher-level reflections, resulting in healthy resolutions and goals, for example by reorganizing information, comparing behavior with personal goals, and by giving users the possibility to explore the data (sources). However, most dashboards are not designed with sufficient understanding of what users need to support them in this process, and simply ‘presenting’ the data is insufficient for reflection to occur. As a result, the higher-level reflection, that enables users to change their behavior, does not occur as often as it possibly could. 
+While many studies mention ideas on how to support reflection, there is little empirical evidence of the effectiveness of these suggestions. Additionally, most of these ideas mentioned in current literature are very abstract recommendations: they are not specific or validated. Thus, it is yet unclear how dashboards should be designed to effectively support users in reaching a higher level of reflection. 
+To enable users of self-tracking technology in taking informed action based on their data, it is essential to better understand how the process of reflection can be supported through dashboard design. Therefore, we will first look into what ‘reflection’ is and present an overview of reflection-supporting methods that are proposed in the literature (Chapter 2). Doing this, we will try to answer the question: “Which reflection-supporting methods are frequently described in the literature?" 
+Afterward, we will continue to evaluate the most promising of these reflection-supporting methods in practice to answer our main research question: “Can a reflection-supporting method built in a data- dashboard contribute to users reaching higher levels of reflection on their data? If so, how?”. The Method of this study will be described in Chapter 3, followed by the Results (Chapter 4), Conclusion, and Discussion (Chapter 5). """
+
 example = """A Framework for Personal Science - Quantified Self. (n.d.). Retrieved June 17, 2021, from https://quantifiedself.com/blog/personal-science/
 Baumer, E. P. S. (2015). Reflective Informatics. 585–594. https://doi.org/10.1145/2702123.2702234
 Baumer, E. P. S., Khovanskaya, V., Matthews, M., Reynolds, L., Sosik, V. S., & Gay, G. (2014). Reviewing reflection: On the use of reflection in interactive system design. Proceedings of the Conference on Designing Interactive Systems: Processes, Practices, Methods, and Techniques, DIS, 93–102. https://doi.org/10.1145/2598510.2598598
@@ -166,8 +182,10 @@ Reitberger, W., Spreicer, W., & Fitzpatrick, G. (2014). Nutriflect: Reflecting c
 Rooksby, J., Rost, M., Morrison, A., & Chalmers, M. (2014). Personal Tracking as Lived Informatics. 1163–1172.
 Wise, A. F., & Jung, Y. (2019). Teaching with analytics: Towards a situated model of instructional decision-making. Journal of Learning Analytics, 6(2), 53–69. https://doi.org/10.18608/jla.2019.62.4"""
 
-
-print(getUrlsSources(example))
+english_stopwords = stopwords.words('english')
+print(wordsSource('These texts can be longer, however then we must find more information etc. etc', set(), english_stopwords))
+# print(countWordsText('These texts can be longer, however then we must find more information etc, etc', english_stopwords))
+# print(getUrlsSources(example))
 # print(sourceIntegration("This is some nice text, is this the correct format?",findWords("This is some nice text, is this the correct format?, some more formatting is required as is the followng format.")))
 # print(findWords(scrape_page("https://dictionary.cambridge.org/dictionary/english/multitasking"), set()))
 # print(scrape_page("https://www.pewresearch.org/internet/2013/01/28/tracking-for-health/"))
