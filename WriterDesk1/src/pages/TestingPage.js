bba82be2256eb526b944a6afaf2563e2cec27978
marklangedijk <m.l.langedijk@student.tue.nl>
2022-06-13 13:43:19 +0200

    Processed comments Rick and Pieter

--- WriterDesk1/src/pages/TestingPage.js
+++ WriterDesk1/src/pages/TestingPage.js
@@ -11,50 +11,8 @@ import { useEffect, useState } from "react";
  * @returns Testing Page
  */
 const TestingPage = () => {
-    const [userLoggedIn, setLoggedIn] = new useState(false);
-    const [testRole, setRole] = new useState('');
-
-    const handleLogin = () => {
-        AuthenticationService.login('m.l.langedijk@student.tue.nl', 'wachtwoord');
-    }
-
-    const handleFLogin = () => {
-        AuthenticationService.login('m.l.langedijk@student.tue.nl', 'Fwachtwoord');
-    }
-
-    const handleRole = () => {
-        AuthenticationService.getRole()
-        .then(result => {
-            setRole(result);
-        }).catch(error =>{
-            setRole('');
-        });
-        //setRole(JSON.parse(localStorage.getItem('currentUser')).role);
-    }
-
-    useEffect(() => {
-        AuthenticationService.checkAuth()
-        .then( () => {
-            setLoggedIn(true);
-        }).catch(error => {
-            setLoggedIn(false);
-        });
-        console.log('useEffect Function is used');
-    });
-
     return (
     <>
-    {testRole != '' &&
-        <p>Role: {testRole}</p>
-    }
-    { userLoggedIn == false &&
-        <Button variant="contained" sx={{bgcolor: 'button.main', color: 'button.text'}} onClick={handleLogin}>Login</Button>
-    }
-    <br/>
-    <Button variant="contained" sx={{bgcolor: 'button.main', color: 'button.text'}} onClick={handleFLogin}>False Login</Button>
-    <br/>
-    <Button variant="contained" sx={{bgcolor: 'button.main', color: 'button.text'}} onClick={handleRole}>Get Role</Button>
-        {testRole != '' ? <> {testRole} </> : <> no role </>}
     </>
     )
 }
