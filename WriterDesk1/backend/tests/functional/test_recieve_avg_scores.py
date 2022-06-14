4468f1cab8a34b75c23b7039a32313135cdb04ff
marklangedijk <m.l.langedijk@student.tue.nl>
2022-06-14 15:07:29 +0200

    Added extra test cases, added comments

--- WriterDesk1/backend/tests/functional/test_recieve_avg_scores.py
+++ WriterDesk1/backend/tests/functional/test_recieve_avg_scores.py
@@ -4,6 +4,21 @@ from datetime import datetime
 import json
 
 def generalGetAvgScore(testClient, userId, avgscoreStyle, avgscoreCohesion, avgscoreStructure, avgscoreIntegration):
+    '''
+        A general method to test retrieving average scores.
+        Check if the average scores that are calculated are equal to given scores
+        Attributes: 
+            data: The data we are trying to test the getAvgScore with.
+            response: Response of the get request.
+            dataResponse: response data, i.e. the average scores
+        Arguments:
+            testClient:  The test client we test this for.
+            userId: userId of user related to this test
+            avgscoreStyle: average language and style score
+            avgscoreCohesion: average cohesion score
+            avgscoreStructure: average structure score
+            avgscoreIntegration: average source integration and content score
+    '''
     data = {
         'userId':userId,
     }
@@ -22,6 +37,21 @@ def generalGetAvgScore(testClient, userId, avgscoreStyle, avgscoreCohesion, avgs
     assert float(dataResponse['scoreIntegration']) == avgscoreIntegration
 
 def testAverageScores(testClient, initDatabaseEmpty):
+    '''
+        This test checks whether we can retreive average scores from the database
+        It first makes files and scores that are needed to calculate average
+        Attributes: 
+            file: file instance 
+            score: score instance
+            userId: user id related to the average scores
+            avgscoreStyle: average language and style score
+            avgscoreCohesion: average cohesion score
+            avgscoreStructure: average structure score
+            avgscoreIntegration: average source integration and content score
+        Arguments:
+            testClient:  The test client we test this for.
+            initDatabaseEmpty: the database instance we test this for. 
+    '''
     del initDatabaseEmpty
 
     # we add files and scores to the database
@@ -63,9 +93,35 @@ def testAverageScores(testClient, initDatabaseEmpty):
         db.session.rollback()
     
     userId = 200
+    # Average scores of the 5 most recent files
     avgscoreStyle = 3
     avgscoreCohesion = 4
     avgscoreStructure = 2
     avgscoreIntegration = 5
 
     generalGetAvgScore(testClient, userId, avgscoreStyle, avgscoreCohesion, avgscoreStructure, avgscoreIntegration)
+
+def testAvgEmptyUser(testClient, initDatabaseEmpty):
+    '''
+        This test checks whether we get an error when we ask the average scores for a user without scores
+        Attributes: 
+            data: The data we are trying to test the getAvgScore with.
+            response: Response of the get request.
+        Arguments:
+            testClient:  The test client we test this for.
+            initDatabaseEmpty: the database instance we test this for. 
+    '''
+    del initDatabaseEmpty
+
+    data = {
+        'userId' : 1,
+    }
+
+    # Retrieve average scores for user which don't have any scores
+    response = testClient.get('/scoreapi/getAvgScores', query_string=data)
+
+    # Check if we get the correct status code
+    assert response.status_code == 400
+
+
+
