df2163203d74626d48d49a59bff60b63fdf4656d
PFvanBeerendonk <64134830+PFvanBeerendonk@users.noreply.github.com>
2022-06-15 13:40:21 +0200

    added some tracking capabilities to testingPage and BlueButton

--- WriterDesk1/src/components/BlueButton.js
+++ WriterDesk1/src/components/BlueButton.js
@@ -1,8 +1,13 @@
 import {
-    Button,
+    Button, tabClasses,
 } from '@mui/material';
 
 import { Link } from 'react-router-dom';
+
+// tracking
+import { useContext } from 'react';
+import { TrackingContext } from '@vrbo/react-event-tracking';
+
 
 /**
  * Default blue action button
@@ -11,16 +16,32 @@ import { Link } from 'react-router-dom';
  * @param {String} idStr id of the button, for testing purposes
  * @param {String} pathName location to which the button will bring you. default is '' and routes you to where you are
  * @param {func} onClick function executes when clicked, default is empty function
+ * @param {style} addStyle Additional style for the button, in the shape of sx
  * 
  * @returns default blue action button
  */
 const BlueButton = ({children, idStr='', pathName='', onClick= ()=>{}, addStyle}) => {
+    // context as given by the Tracking Provider
+    const tc = useContext(TrackingContext);
+    
+    const handleClick = () => {
+        if (tc.hasProvider) {
+            tc.trigger(`button.click`)
+        } else {
+            // no provider available, error
+            console.log('No Provider Available, error')
+        }
+
+        // usual button action
+        onClick()
+    }
+    
     return (
         <Button 
             id={idStr}
             size='large' variant='contained' sx={[{ bgcolor: 'button.main', color: 'button.text' }, addStyle]} 
             component={Link} to={{pathname: pathName}}
-            onClick={onClick}
+            onClick={handleClick}
         >
             {children}
         </Button>
--- WriterDesk1/src/pages/TestingPage.js
+++ WriterDesk1/src/pages/TestingPage.js
@@ -1,7 +1,20 @@
 import {
-    
+    Button
 } from "@mui/material";
+import BlueButton from "./../components/BlueButton";
 import TestingComponent from "./../components/TestingComponent";
+
+// trackers
+import {TrackingProvider, TrackingContext} from '@vrbo/react-event-tracking';
+import { useContext } from "react";
+
+const triggerFunc = () => {
+    console.log('TRIGGERED')
+}
+
+// click data tracking
+
+
 
 /**
  * page used for testing, to be removed later
@@ -9,15 +22,41 @@ import TestingComponent from "./../components/TestingComponent";
  * @returns Testing Page
  */
 const TestingPage = () => {
+    const defaultFields = {location: 'top-right'};
+    const defaultOptions = {asynchronous: true};
+    const customTrigger = (event, fields, options) => {
+        // Implement custom event tracking.
+        console.log('ev',event)
+        console.log('f',fields)
+        console.log('op',options)
+        triggerFunc()
+    }
+    
+    const eventPayload = {
+        'generic.click': {
+            eventaction: 'toggle'
+        },
+        'button.click': {
+            eventaction: 'button clicked'
+        },
+        'generic.event': {
+            eventaction: 'hit test'
+        }
+    }
 
     return (
         <>
-            TestingPage
+            <TrackingProvider
+                fields={defaultFields}
+                options={defaultOptions}
+                trigger={customTrigger}
+                eventPayload={eventPayload}
+            >
+                <BlueButton onClick={() => {console.log("I'm Blue")}}> blueee </BlueButton>
+            </TrackingProvider>
         </>
     );
 }
-
-
 
 
 export default TestingPage;
