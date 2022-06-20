826ff16659e0e6536309cffb461d0628f77084cb
PFvanBeerendonk <64134830+PFvanBeerendonk@users.noreply.github.com>
2022-06-20 21:02:14 +0200

    move trackingWrapper to index

--- WriterDesk1/src/components/Base.js
+++ WriterDesk1/src/components/Base.js
@@ -32,10 +32,6 @@ import LogoDevIcon from '@mui/icons-material/LogoDev'; //replace with logo?;
 // routing
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 
-// tracking
-import TrackingWrapper from "./../services/TrackingWrapper";
-import { history } from '../helpers/history';
-
 //Width of the opened drawer
 const drawerWidth = 240;
 
@@ -169,112 +165,108 @@ const Base = ({
         setAdmin(true);
       }
       if (user.role === 'researcher' || user.role === 'admin') {
-        setResearcher(true); 
+        setResearcher(true);
       }
     }
-  },[]);
+  }, []);
 
   return (
-    // supply wrapper as TrackerProvider, 
-    // any object inside can use TrackerContext
-    <TrackingWrapper>
-      <Box sx={{ display: 'flex' }} color="textPrimary" className='baseRoot'>
-        <CssBaseline />
-        <CustomAppBar
-          position='fixed'
-          sx={{
-            bgcolor: 'appBar.background',
-            ...(open && { width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }),
-            ...(!open && {
-              width: enableNav ? `calc(100% - ${theme.spacing(7)} + 1px)` : `100%`,
-              [theme.breakpoints.up('sm')]: {
-                width: enableNav ? `calc(100% - ${theme.spacing(8)} + 1px)` : `100%`,
-              },
-            })
-          }}
-        >
-          <Toolbar sx={{
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-          }}>
-            <Settings style={{ opacity: '0', margin: '8' }} />
-            <Typography variant="h6" component="div" sx={{ color: "appBar.text", }}> {title} </Typography>
-            <IconButton
-              sx={{
-                justifySelf: "flex-end",
-                color: "appBar.icon",
-              }}
-              component={Link} to='settings'
-            >
-              <Person />
-            </IconButton>
-          </Toolbar>
-        </CustomAppBar>
+    <Box sx={{ display: 'flex' }} color="textPrimary" className='baseRoot'>
+      <CssBaseline />
+      <CustomAppBar
+        position='fixed'
+        sx={{
+          bgcolor: 'appBar.background',
+          ...(open && { width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }),
+          ...(!open && {
+            width: enableNav ? `calc(100% - ${theme.spacing(7)} + 1px)` : `100%`,
+            [theme.breakpoints.up('sm')]: {
+              width: enableNav ? `calc(100% - ${theme.spacing(8)} + 1px)` : `100%`,
+            },
+          })
+        }}
+      >
+        <Toolbar sx={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+          <Settings style={{ opacity: '0', margin: '8' }} />
+          <Typography variant="h6" component="div" sx={{ color: "appBar.text", }}> {title} </Typography>
+          <IconButton
+            sx={{
+              justifySelf: "flex-end",
+              color: "appBar.icon",
+            }}
+            component={Link} to='settings'
+          >
+            <Person />
+          </IconButton>
+        </Toolbar>
+      </CustomAppBar>
 
-        <CustomDrawer
-          variant="permanent"
+      <CustomDrawer
+        variant="permanent"
+        open={open}
+        sx={{
+          display: enableNav ? 'initial' : 'none',
+          bgcolor: 'drawer.background',
+          height: '100%',
+        }}
+      >
+        <CustomDrawerHeader
           open={open}
           sx={{
-            display: enableNav ? 'initial' : 'none',
             bgcolor: 'drawer.background',
-            height: '100%',
           }}
         >
-          <CustomDrawerHeader
-            open={open}
-            sx={{
-              bgcolor: 'drawer.background',
-            }}
-          >
-            <Tooltip title="Menu">
-              <IconButton
-                sx={{
-                  color: 'drawer.burger',
-                  bgcolor: 'inherit',
-                }}
-                aria-label="open drawer"
-                onClick={handleDrawer}
-                edge={false}
-              >
-                <Menu />
-              </IconButton>
-            </Tooltip>
-          </CustomDrawerHeader>
-          <Divider sx={{ bgcolor: 'drawer.divider', }} />
-          <List
-            sx={{
-              bgcolor: 'drawer.background',
-              height: '100%',
-            }}>
-            <NavigationLink open={open} text="Main" Icon={LogoDevIcon} allowed={enableNav} pageLink='Main' />
-            <NavigationLink open={open} text="Upload" Icon={FileUpload} allowed={enableNav} pageLink='Upload' />
-            <NavigationLink open={open} text="Progress" Icon={Timeline} allowed={enableNav} pageLink='Progress' />
-            <NavigationLink open={open} text="Documents" Icon={Article} allowed={enableNav} pageLink='Documents' />
-            <Divider sx={{
-              bgcolor: 'drawer.divider',
-              display: admin || researcher ? 'block' : 'none'
-            }} />
-            <NavigationLink open={open} text="Participants" Icon={Group} allowed={researcher | admin} pageLink='Participants' />
-            <NavigationLink open={open} text="Projects" Icon={ListAlt} allowed={researcher | admin} pageLink='Projects' />
-            <NavigationLink open={open} text="Feedback Models" Icon={Build} allowed={researcher | admin} pageLink='FeedbackModels' />
-            <Divider sx={{
-              bgcolor: 'drawer.divider',
-              display: admin ? 'block' : 'none'
-            }} />
-            <NavigationLink open={open} text="Users" Icon={Settings} allowed={admin} pageLink='Users' />
-          </List>
-        </CustomDrawer>
+          <Tooltip title="Menu">
+            <IconButton
+              sx={{
+                color: 'drawer.burger',
+                bgcolor: 'inherit',
+              }}
+              aria-label="open drawer"
+              onClick={handleDrawer}
+              edge={false}
+            >
+              <Menu />
+            </IconButton>
+          </Tooltip>
+        </CustomDrawerHeader>
+        <Divider sx={{ bgcolor: 'drawer.divider', }} />
+        <List
+          sx={{
+            bgcolor: 'drawer.background',
+            height: '100%',
+          }}>
+          <NavigationLink open={open} text="Main" Icon={LogoDevIcon} allowed={enableNav} pageLink='Main' />
+          <NavigationLink open={open} text="Upload" Icon={FileUpload} allowed={enableNav} pageLink='Upload' />
+          <NavigationLink open={open} text="Progress" Icon={Timeline} allowed={enableNav} pageLink='Progress' />
+          <NavigationLink open={open} text="Documents" Icon={Article} allowed={enableNav} pageLink='Documents' />
+          <Divider sx={{
+            bgcolor: 'drawer.divider',
+            display: admin || researcher ? 'block' : 'none'
+          }} />
+          <NavigationLink open={open} text="Participants" Icon={Group} allowed={researcher | admin} pageLink='Participants' />
+          <NavigationLink open={open} text="Projects" Icon={ListAlt} allowed={researcher | admin} pageLink='Projects' />
+          <NavigationLink open={open} text="Feedback Models" Icon={Build} allowed={researcher | admin} pageLink='FeedbackModels' />
+          <Divider sx={{
+            bgcolor: 'drawer.divider',
+            display: admin ? 'block' : 'none'
+          }} />
+          <NavigationLink open={open} text="Users" Icon={Settings} allowed={admin} pageLink='Users' />
+        </List>
+      </CustomDrawer>
 
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }} >
-          <CustomDrawerHeader />
-          <Box className='content' sx={{ height: '93.5%' }}>
-            <Outlet context={{ setTitle }} />
-          </Box>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }} >
+        <CustomDrawerHeader />
+        <Box className='content' sx={{ height: '93.5%' }}>
+          <Outlet context={{ setTitle }} />
         </Box>
       </Box>
-    </TrackingWrapper>
+    </Box>
   );
 }
 
