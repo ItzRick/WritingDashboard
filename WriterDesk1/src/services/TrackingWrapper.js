4403097e5076d5106642ca91da2bb896cfcbe91e
PFvanBeerendonk <64134830+PFvanBeerendonk@users.noreply.github.com>
2022-06-20 22:55:14 +0200

    send click

--- WriterDesk1/src/services/TrackingWrapper.js
+++ WriterDesk1/src/services/TrackingWrapper.js
@@ -12,9 +12,21 @@ import {authHeader} from "./../helpers/auth-header";
  */
 const TrackingWrapper = ({children}) => {
     
+
+    /**
+     * 
+     * @param {String} url url of the page where the click happened
+     * @param {String} eventType type of event, can be one of [click.button, click.link, view.document]
+     * @param {String} buttonId id of the button, usually similair to the text displayed on the button, not available for view.document events
+     * @param {int} documentId id of the document being viewed, only availabel for view.document events
+     * @param {String} documentName name of the document being viewed, only availabel for view.document events
+     */
     const sendClick = ({
         url,
-        
+        eventType,
+        buttonId,
+        documentId,
+        documentName,
     }) => {
         console.log(url)
         //url for request
@@ -23,6 +35,10 @@ const TrackingWrapper = ({children}) => {
         // create form with all the file information
         const formData = new FormData();
         formData.append('url', url);
+        formData.append('eventType', eventType);
+        formData.append('buttonId', buttonId);
+        formData.append('buttonId', documentId);
+        formData.append('buttonId', documentName);
 
         //post the file
         axios.post(requestUrl, formData, {
@@ -39,7 +55,8 @@ const TrackingWrapper = ({children}) => {
             url: window.location.href,
             eventType: event.eventType,
             buttonId: event.buttonId,
-            linkPath: event.linkPath,
+            documentId: event.documentId,
+            documentName: event.documentName,
         })
     }
 
