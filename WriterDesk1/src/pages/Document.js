eeee3bb359906fd53b14a9f534e72e9881d28e03
Basarendsen <b.s.arendsen@student.tue.nl>
2022-06-20 16:51:04 +0200

    Create buttons for explanations

--- WriterDesk1/src/pages/Document.js
+++ WriterDesk1/src/pages/Document.js
@@ -297,7 +297,8 @@ function Document() {
           ]}
           // The title of the char is 'scores':
           layout={{
-            title: 'Scores',
+            // title: 'Scores',
+            margin: {l: 50, r: 50, b: 40, t: 30, pad: 4},
             // Scores can be between 0 and 10, so the y-axis range is set accordingly:
             yaxis: {
               range: [0, 10],
@@ -318,7 +319,7 @@ function Document() {
           }}
           // So the chart can resize:
           useResizeHandler={true}
-          style={{ width: '100%', height: '50%' }}
+          style={{ width: '100%', height: '300px' }}
           onHover={e => {
             e.event.target.style.cursor = 'pointer' // Changes cursor on hover to pointer
           }}
@@ -326,6 +327,15 @@ function Document() {
             e.event.target.style.cursor = 'default' // Change cursor back on unhover
           }}
         />
+
+        <hr className="horizontalline"/>
+
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+         <button className="buttonShowExplanationsStyle" onClick={e => showAllExplanationsOfType(0)}>Language & Style</button>
+        <button className="buttonShowExplanationsCohesion" onClick={e => showAllExplanationsOfType(1)}>Cohesion</button>
+        <button className="buttonShowExplanationsStructure" onClick={e => showAllExplanationsOfType(2)}>Structure</button>
+        <button className="buttonShowExplanationsIntegration" onClick={e => showAllExplanationsOfType(3)}>Source Integration & Content</button>
+        </div>
 
         <br />
 
