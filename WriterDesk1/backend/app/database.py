05d43818d5fdcff8bfd54706e4af58cc9a02bf4c
PFvanBeerendonk <64134830+PFvanBeerendonk@users.noreply.github.com>
2022-06-20 14:19:47 +0200

    project test fixes

--- WriterDesk1/backend/app/database.py
+++ WriterDesk1/backend/app/database.py
@@ -1,5 +1,4 @@
 from app import db
-from app import models
 from app.models import User, Scores, Files, Projects, ParticipantToProject
 
 # helper function, TODO remove before deploy
@@ -10,7 +9,9 @@ def initialSetup():
     db.create_all()
     
 	# create admin user
-    u = User(username='admin', password_plaintext='admin')
+    u = User.query.filter_by(username='admin').first()
+    if u is None:
+        u = User(username='admin', password_plaintext='admin')
     u.role = 'admin'
     uploadToDatabase(u)
 
@@ -86,3 +87,112 @@ def postUser(username, password):
     return True
 
 
+
+def postParticipant(username, password):
+    '''
+        This function handles the query that generates a partipant account. When there is no participant present in the database with the given username,
+        a new participant is posted in the database with a unique id, the given username, the participant role and a hash of the given password.
+        Attributes:
+            user: user object that is to be added to the database
+        Arguments:
+            username: username as given in frontend
+            password: password as given in frontend
+        Return:
+            Returns the user when a new user was added to the database
+    '''
+
+    # Check if there is already a user with this username
+    if db.session.query(User).filter_by(username=username).count() > 0:
+        raise Exception('User exists already')
+
+    # Add user to the database with participant role
+    user = User(username=username, password_plaintext=password, role="participant")
+    db.session.add(user)
+    db.session.flush()
+    return user
+
+def postParticipantToProject(userId, projectId):
+    '''
+        This function handles the query that creates an entry in ParticipantToProject, to link a participant to a research project. 
+        Attributes:
+            project: query result to check if there exists a project with the given id
+            dataTuple: object that is to be added to the database
+        Arguments:
+            userId: id of the participant
+            projectId: id of the research project
+    '''
+    project = Projects.query.filter_by(id=projectId).all()
+    if len(project) == 0:
+        raise Exception('Project does not exist')
+
+    dataTuple = ParticipantToProject(userId=userId, projectId=projectId)
+    db.session.add(dataTuple)
+    db.session.flush()
+
+def getProjectsByResearcher(user):
+    '''
+        This function handles the query for retrieving a user's projects.
+        Attributes:
+            projects: result of the query, containing the projects of the given user
+        Arguments:
+            user: id of the user whose projects need to be retrieved
+        Return:
+            Returns list of projects of the given user
+    '''
+    # Retrieve the projects of the user
+    projectList = Projects.query.filter_by(userId=user).all()
+    projects = [dict(userId = proj.userId, projectName = proj.projectName, id = proj.id) for proj in projectList]
+    return projects 
+
+def getParticipantsByResearcher(user):
+    '''
+        This function handles the query for retrieving a user's participants.
+        Attributes:
+            userList: a query that gets the user information
+            projectList: a query that gets the projects of the user
+            participantIdList: a query that gets the participant ids of the projects of the user
+            participantList: retrieve the list of participants
+            participants: result of the query, containing the participants of the given user
+        Arguments:
+            user: id of the user whose participants need to be retrieved
+        Return:
+            Returns list of participants of the given user
+    '''
+    # Get the query that gets the user information
+    userList = User.query.filter_by(id=user).subquery()
+    # Get the query that gets the projects of the user
+    projectList = Projects.query.join(userList, Projects.userId == userList.c.id).subquery()
+    # Get the query that gets the participant ids of the projects of the user
+    participantIdList = ParticipantToProject.query.join(projectList, ParticipantToProject.projectId == projectList.c.id).subquery()
+    # Get the participants of the projects of the user
+    participantList = User.query.join(participantIdList, User.id == participantIdList.c.userId).all()
+    participants = [dict(role = part.role, id = part.id, username = part.username, passwordHash = part.passwordHash) for part in participantList]
+    # Return the information of the participants in all projects of the user
+    return participants
+    
+def getParticipantsWithProjectsByResearcher(user):
+    '''
+        This function handles the query for retrieving a user's participants, including the projects they correspond to.
+        Attributes:
+            projectParticipantConnection: the connection between the participant and the project
+            projectid: the id of the project that the participant is in
+            project: the project that the participant is in
+            projectname: the name of the project that the participant is in
+            participants: result of the query, containing the participants of the given user
+        Arguments:
+            user: id of the user whose participants need to be retrieved
+        Return:
+            Returns list of participants of the given user including the projects they correspond to
+    '''
+    participants = getParticipantsByResearcher(user)
+    
+    for participant in participants:
+        projectParticipantConnection = ParticipantToProject.query.filter_by(userId=participant['id']).all()
+        projectid = projectParticipantConnection[0].projectId
+        project = Projects.query.filter_by(id=projectid).all()
+        projectname = project[0].projectName
+        participant['projectid'] = projectid
+        participant['projectname'] = projectname
+
+    # Return the information of the participants in all projects of the user
+    return participants
--- WriterDesk1/backend/app/projectapi/routes.py
+++ WriterDesk1/backend/app/projectapi/routes.py
@@ -3,13 +3,35 @@ from app.projectapi import bp
 from flask import request, jsonify
 from app.models import Projects
 
-from app.database import uploadToDatabase, removeFromDatabase, getParticipantsByResearcher, getProjectsByResearcher
+from app.database import uploadToDatabase, removeFromDatabase, getProjectsByResearcher, getParticipantsWithProjectsByResearcher
 from app import generateParticipants as gp
 from app import db
 
+import os
+import shutil
+
+from app.projectapi import bp
+
+from flask import request, jsonify, current_app
+from app.models import Projects, User, ParticipantToProject
+
+from app.database import uploadToDatabase, removeFromDatabase, getParticipantsByResearcher, getProjectsByResearcher
+from app import generateParticipants as gp
+from app import db
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import current_user
+
+
+
 @bp.route('/addparticipants', methods=["POST"])
 def addParticipantsToExistingProject():
-
+    '''
+    This function handles the creation of new participants and adding them to an existing project.
+    It raises an error when participants were not added to database.
+    Attributes:
+        count: the number of participants that should be added
+        projectId: the id of the project the participants should be added to
+    '''
     # Retrieve data from request
     count = int(request.json.get("count", None))
     projectId = int(request.json.get("projectid", None))
@@ -22,24 +44,24 @@ def addParticipantsToExistingProject():
         return str(e), 400
 
 @bp.route('/setProject', methods=['POST'])
+@jwt_required()
 def setProject():
     '''
     This function handles the creation of research projects using a user id and a project name.
     Attributes:
-        userId: user id as given by the frontend
         projectName: project name as given by the frontend
-    Arguments:
+        current_user: the user currently logged in
         projectIndb: project object that is uploaded to the database
     '''
     # Get the data as sent by the react frontend:
-    userId = request.form.get('userId')
     projectName = request.form.get('projectName')
 
-    if userId is None:
-        return 'userId not specified', 400
+    if User.query.filter_by(id=current_user.id).first().role == 'student' \
+            or User.query.filter_by(id=current_user.id).first().role == 'participant':
+        return 'User is not admin or researcher', 400
 
     # create Projects object
-    projectIndb = Projects(userId=userId, projectName=projectName)
+    projectIndb = Projects(userId=current_user.id, projectName=projectName)
 
     # Upload row to database
     db.session.add(projectIndb)
@@ -50,27 +72,58 @@ def setProject():
 
 
 @bp.route('/deleteProject', methods=['DELETE'])
+@jwt_required()
 def deleteProject():
     '''
-    This function handles the deletion of research projects using the corresponding project id's.
+    This function handles the deletion of research projects using the corresponding project id's. If the project
+    does not exist in the database, the function raises a 404 error. If the project has a different user than the current
+    user, the function raises a 400 error.
     Attributes:
         projectIds: List of project id's as given by the frontend
-    Arguments:
         projectToBeRemoved: project object that is going to be removed
     '''
     # Get the data as sent by the react frontend:
     projectIds = request.form.getlist('projectId')
 
     for projectId in projectIds:
+        if Projects.query.filter_by(id=projectId).first() is None:
+            return 'project does not exist in database', 404
+
+        # Check if the project that is going to be deleted is related to the current user
+        if current_user.id != Projects.query.filter_by(id=projectId).first().userId:
+            return 'Project is not related to current user', 400
+
+    DeleteAllFilesFromProject(projectIds)  # Remove all files corresponding to the project ids from the server
+
+    for projectId in projectIds:
         # Retrieve the row that needs to be removed
         projectToBeRemoved = Projects.query.filter_by(id=projectId).first()
-
-        if projectToBeRemoved is None:
-            return 'project does not exist in database', 404
 
         # Remove row from database
         removeFromDatabase(projectToBeRemoved)
 
+    return 'success', 200
+
+
+def DeleteAllFilesFromProject(projectIds):
+    '''
+    This function handles the deletion of files corresponding to all users from a project.
+    Attributes:
+        users: Users corresponding to project removed
+        folderToRemove: path of folder that needs to be removed
+    Arguments:
+        projectIds: List of project id's as given by the frontend
+    '''
+
+    for projectId in projectIds:
+        # Retrieve users of project with project id
+        users = ParticipantToProject.query.filter_by(projectId=projectId).all()
+        for user in users:
+            try:
+                folderToRemove = os.path.join(current_app.config['UPLOAD_FOLDER'], str(user.userId))
+                shutil.rmtree(folderToRemove)  # Try to remove folder recursively
+            except FileNotFoundError:
+                print('Folder not found')
     return 'success', 200
 
 @bp.route('/viewparticipantsofuser', methods=["GET"])
@@ -86,7 +139,7 @@ def viewParticipantsOfUser():
     # Get the user id as sent by the react frontend
     userId = request.args.get('userId')
     # Retrieve the information from the participants corresponding to the projects of the user
-    participants = getParticipantsByResearcher(userId)
+    participants = getParticipantsWithProjectsByResearcher(userId)
     # Throw an error if the project variable is empty
     # in other words, if the user has no projects
     if participants == []:
