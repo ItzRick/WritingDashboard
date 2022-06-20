8c423cfd4c6f5bd589933f964bdb67f64389c52a
PFvanBeerendonk <64134830+PFvanBeerendonk@users.noreply.github.com>
2022-06-20 14:20:17 +0200

    added getParticipantsProjects and tests

--- WriterDesk1/backend/app/usersapi/routes.py
+++ WriterDesk1/backend/app/usersapi/routes.py
@@ -1,7 +1,8 @@
-from flask import jsonify
+from flask import request, jsonify
+from app import db
 
-from app.database import getUsers
-from app.models import User
+from app.database import getUsers, getParticipantsWithProjectsByResearcher
+from app.models import User, Projects, ParticipantToProject
 from app.usersapi import bp
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import current_user
@@ -25,3 +26,30 @@ def usersRetrieve():
         return jsonify(users)
     else:
         return 'No user available', 400
+
+@bp.route('/getParticipantsProjects', methods=['GET'])
+@jwt_required()
+def getParticipantsProjects():
+    '''
+    This function handles retrieving the participants and related for a particular user
+    The user must be an admin or researcher to access this method
+    TODO: descibe return data
+    Attributes:
+        researcherId: id of the researcher or admin calling this function
+    Return:
+        Returns success if it succeeded, or an 
+        error message:
+            403, if the current_user is neither an admin nor a researcher
+    '''
+    # check if user is admin OR researcher (i.e. reject if user is neither)
+    if current_user.role != 'admin' and current_user.role != 'researcher':
+        return 'Method only accessible for admin or researcher users', 403
+    
+
+    # get researcher id
+    researcherId = current_user.id
+
+    # retrieve researcher data
+    data = getParticipantsWithProjectsByResearcher(researcherId)
+    # return data
+    return jsonify(data), 200
