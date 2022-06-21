7ef0304806e5bb20c001d4f454426e8554b2857a
Basarendsen <b.s.arendsen@student.tue.nl>
2022-06-21 13:06:02 +0200

    Max course code length of 16 in frontend

--- WriterDesk1/src/components/UploadSingleFile.js
+++ WriterDesk1/src/components/UploadSingleFile.js
@@ -203,7 +203,7 @@ const UploadSingleFile = forwardRef(({ setUploadSingleFiles, thisIndex }, ref) =
                         renderInput={(params) => <TextField {...params} />}
                     />
                 </LocalizationProvider>
-                <TextField label='Course ID' variant='outlined' value={course} onChange={event => setCourse(event.target.value)} />
+                <TextField label='Course ID' inputProps={{ maxLength: 16 }} variant='outlined' value={course} onChange={event => setCourse(event.target.value)} />
                 <Button variant='contained' sx={{ bgcolor: 'buttonWarning.main', color: 'buttonWarning.text', ml: '5px',}} value={thisIndex} onClick={removeInstance}>Remove</Button>
             </div>
             {displayAlertType ? <Alert severity="error">Upload a file with a .txt, .pdf or .docx filetype!</Alert> : null}
