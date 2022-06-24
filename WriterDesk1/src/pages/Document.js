e173b98c8da433ddab78306bceda04aca2ad763b
Basarendsen <b.s.arendsen@student.tue.nl>
2022-06-24 23:22:02 +0200

    Retrieve explanations from the database when clicking on it.

--- WriterDesk1/src/pages/Document.js
+++ WriterDesk1/src/pages/Document.js
@@ -41,10 +41,13 @@ function Document() {
   const [scoreCohesion, setScoreCohesion] = useState();
   const [scoreIntegration, setScoreIntegration] = useState();
 
+  const [highlights, setHighlights] = useState([]);
   const [explanations, setExplanations] = useState([]);
 
   // context as given by the Tracking Provider
   const tc = useContext(TrackingContext);
+
+  const [fileID, setFileID] = useState();
 
   useEffect(() => {
     setTitle('Document');
@@ -52,6 +55,7 @@ function Document() {
 
   useEffect(() => {
     const fileId = location.state.fileId; // Get file id from previous page.
+    setFileID(fileId);
     fetchFilePath(fileId); // Set file path and type
     fetchScores(fileId); // Set scores of current file
     fetchExplanations(fileId); //Set mistakes of current file
@@ -110,7 +114,7 @@ function Document() {
           // Append explanation to array
           explanationsArray = [...explanationsArray, response.data[i]];
         }
-        setExplanations(explanationsArray);
+        setHighlights(explanationsArray);
       })
   }
 
@@ -126,82 +130,38 @@ function Document() {
     let x = coords[0] + e.clientX - rect.left; // x-coordinate of click in document
     let y = coords[1] + e.clientY - rect.top; // y-coordinate of click in document
 
-    let newArrShowTextbox = []; // Create new array to overwrite showTextbox
+    // Make the call to the backend:
+    axios.get('https://127.0.0.1:5000/scoreapi/getExplanationForFileAndCoordinates', { params: { fileId: fileID, x: x, y: y } })
+      .then((response) => {
+        // Set explanations to show explanation boxes
+        setExplanations(response.data);
 
-    for (let i = 0; i < explanations.length; i++) {
-      // Loop over all mistakes to check if there is clicked on that mistake.
-      let left = explanations[i].X1 - 2; // x1 of mistake[i] coordinates
-      let right = explanations[i].X2 + 2; // x2 of mistake[i] coordinates
-      let top = explanations[i].Y1; // y1 of mistake[i] coordinates
-      let bottom = explanations[i].Y2; // y2 of mistake[i] coordinates
+        // Handle tracking
+        if (tc.hasProvider) {
+        // Set trigger with explanations type
+          tc.trigger({
+            eventType: 'click.highlight',
+            buttonId: response.data[0].type,
+          })
+        }
+      })
 
-      //Set showTextbox true for every mistake that is clicked
-      const hasBeenClicked = (left <= x) && (x <= right) && (top <= y) && (y <= bottom);
-      newArrShowTextbox[i] = hasBeenClicked;
-      //handle tracking
-      if (tc.hasProvider && hasBeenClicked) {
-        // set trigger with explanations type
-        tc.trigger({
-          eventType: 'click.highlight',
-          buttonId: explanations[i].type,
-        })
-      }
-    }
-    setShowTextbox(newArrShowTextbox); // Overwrite showTextbox
-  }
-
-  /**
-   * Function to get all unique rows of an object by the given keys.
-   * @param {Object} arr - Array of dictionary rows to get only unique rows.
-   * @param {Object} keyProps - Array of keys to base the unique rows on.
-   * @return Modified array that only has all unique rows based on the given keys.
-   */
-  const unique = (arr, keyProps) => {
-   const kvArray = arr.map(entry => {
-     const key = keyProps.map(k => entry[k]).join('|');
-     return [key, entry];
-   });
-   const map = new Map(kvArray);
-   return Array.from(map.values());
   }
 
 
+
+
   /**
-   * Function to show or hide all the explanations of one type. Call this function when clicking on the bar chart.
+   * Function to show all the explanations of one type.
    * @param {number} type - Number for type of mistake
    */
   const showAllExplanationsOfType = (type) => {
-    // Copy explanations array to newExpl
-    const newExpl = explanations.map(obj => ({...obj}));
-
-    // Add number to new array to distinguish after removing rows
-    for (let i = 0; i < newExpl.length; i++) {
-      newExpl[i].number = i;
-    }
-
-    const keys = ['explanation', 'mistakeText', 'type', 'replacement1', 'replacement2', 'replacement3'];
-    const uniqueExpl = unique(newExpl, keys);  // Get all unique explanations to prevent showing duplicates
-
-    let allOpenOfType = true; // Boolean that indicate if all explanations of given type are shown
-
-    // Loop over all unique explanations
-    for (const expl of uniqueExpl) {
-      // Check if explanation of mistake is shown
-      if (expl.type === type && !showTextbox[expl.number]) {
-        allOpenOfType = false; // Not all explanations of the given type are shown
-      }
-    }
-
-    let newArrShowTextbox = []; // Create new array to overwrite showTextbox
-
-    for (const expl of uniqueExpl) {
-      if (expl.type === type) {
-        // Show all explanations of the given type
-        // If this is already the case, hide them all
-        newArrShowTextbox[expl.number] = !allOpenOfType;
-      }
-    }
-    setShowTextbox(newArrShowTextbox); // Overwrite showTextbox
+    // Make the call to the backend
+    axios.get('https://127.0.0.1:5000/scoreapi/getExplanationForFileAndType', { params: { fileId: fileID, type: type } })
+      .then((response) => {
+        // Set explanations to show explanation boxes
+        setExplanations(response.data);
+      })
   }
 
   // tracking
@@ -223,7 +183,7 @@ function Document() {
   const TextBoxExplanation = (props) => {
     return (<>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <div className={showTextbox[props.number] ? 'textBoxExpl' : 'hidden'} id={'textBoxExpl' + props.number}
+        <div className={'textBoxExpl'} id={'textBoxExpl' + props.number}
           style={{ backgroundColor: typeToColor(props.type), borderColor: typeToColor(props.type) }}>
           <Typography className='textBoxType' style={{ color: typeToColor(props.type), fontSize: 'calc(8px + 0.5vw)' }}>
             <b>{typeToName(props.type)}</b>
@@ -325,7 +285,7 @@ function Document() {
 
 
   return (
-    <>
+    <div>
       <div className="all-page-container" id="all-page-container" style={{ width: '50%' }}>
         {/** potentially convert document to pdf and show document on page */}
         <AllPagesPDFViewer
@@ -333,10 +293,10 @@ function Document() {
           docId={location.state.fileId}
           docName={location.state.fileName}
         />
-        {explanations.map((explanation, i) =>
+        {highlights.map((highlight, i) =>
           <ClickableTextDiv
-            key={explanation.explId} number={i}
-            coords={[explanation.X1, explanation.Y1, explanation.X2, explanation.Y2]} type={explanation.type}
+            key={highlight.explId} number={i}
+            coords={[highlight.X1, highlight.Y1, highlight.X2, highlight.Y2]} type={highlight.type}
           />
         )}
       </div>
@@ -412,7 +372,7 @@ function Document() {
           />
         )}
       </div>
-    </>
+    </div>
   );
 }
 
