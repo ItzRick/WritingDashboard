6944eb9dfa6f482b9e07d4d351b64fae7b1334bc
RIck Overmeer <r.j.overmeer@student.tue.nl>
2022-06-24 10:48:27 +0200

    fix int to float

--- WriterDesk1/backend/tests/functional/test_getCurrentFeedbackVersion.py
+++ WriterDesk1/backend/tests/functional/test_getCurrentFeedbackVersion.py
@@ -9,8 +9,8 @@ def testGetCurrentFeedbackVersion(testClient):
             testClient:  The test client we test this for. 
     '''
     # Set the FEEDBACKVERSION to a known value:
-    current_app.config['FEEDBACKVERSION'] = '1.00'
+    current_app.config['FEEDBACKVERSION'] = '0.01'
     response = testClient.get('/feedback/getCurrentVersion')   
     # Test if we also correctly retrieve this feedback version:
     assert response.status_code == 200
-    assert response.data == b'1.00'
+    assert response.data == b'0.01'
