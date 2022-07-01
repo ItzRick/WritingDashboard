d6cacb54fc9e1f52b8b6cf9e071f879db43a915b
Basarendsen <b.s.arendsen@student.tue.nl>
2022-07-01 14:11:04 +0200

    added test case

--- WriterDesk1/backend/tests/functional/test_delete_projects.py
+++ WriterDesk1/backend/tests/functional/test_delete_projects.py
@@ -1,6 +1,11 @@
-from app.models import Projects
+from app.models import Projects, User, ParticipantToProject, Files
 from app.database import uploadToDatabase, removeFromDatabase
 from app import db
+import os
+
+from datetime import date
+
+from flask import current_app
 
 from test_set_role import loginHelper
 
@@ -148,3 +153,63 @@ def testRemoveFromDatabaseInvalidProject(testClient, initDatabase):
 
     # Check if we get the correct status_code:
     assert response.status_code == 404
+
+
+def testDeleteFilesFromProject(testClient, initDatabase):
+    '''
+        Test if the files folder of a participant is removed when the project of the participant is removed.
+        Arguments:
+            testClient: The test client we test this for.
+            initDatabase: The database instance we test this for.
+        Attributes:
+            response: The response of the deleteProject backend call
+            access_token: Access token for user ad min
+            adId: ID of the user ad min
+            participant: participant that is added to the database
+            project: project that is added to the database
+            partToProject: ParticipantToProject instance that is added the database
+            filepath: Path of the files folder for the participant par_1
+    '''
+    del initDatabase
+
+    # Get access token for ad min
+    access_token = loginHelper(testClient, 'ad', 'min')
+    # Get userId for ad min
+    adId = User.query.filter_by(username='ad').first().id
+
+    # Create participant in the database
+    participant = User(username='par_1', password_plaintext='par_1', role='participant')
+    uploadToDatabase(participant)
+
+    # Create the project in the database
+    project = Projects(userId=adId, projectName='Project1')
+    uploadToDatabase(project)
+
+    # Create the ParticipantToProject instance in the database
+    partToProject = ParticipantToProject(userId=participant.id, projectId=project.id)
+    uploadToDatabase(partToProject)
+
+    # Path of the folder for the participant
+    filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], str(participant.id))
+
+    # Create directory
+    os.mkdir(filepath)
+
+    # Check if directory exists
+    assert os.path.exists(filepath)
+
+    # Try to delete the project from the database
+    response = testClient.delete('/projectapi/deleteProject', data={'projectId': project.id},
+                                 headers={"Authorization": "Bearer " + access_token})
+
+    # Check if the everything is removed from the database
+    assert Projects.query.filter_by(projectName='Project1').first() is None
+    assert User.query.filter_by(username='par_1').first() is None
+    assert ParticipantToProject.query.filter_by(projectId=project.id).first() is None
+
+    # Check status code
+    assert response.status_code == 200
+    assert response.data == b'success'
+
+    # Check if path is removed
+    assert not os.path.exists(filepath)
