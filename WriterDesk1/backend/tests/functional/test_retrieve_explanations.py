c3ac1db23bf7ba2e5598f5ca2f4ce60aaba77ed4
Basarendsen <b.s.arendsen@student.tue.nl>
2022-07-01 14:33:25 +0200

    added test cases

--- WriterDesk1/backend/tests/functional/test_retrieve_explanations.py
+++ WriterDesk1/backend/tests/functional/test_retrieve_explanations.py
@@ -218,3 +218,129 @@ def testGetExplanationByFileAndTypeError(testClient, initDatabaseEmpty):
 
     # Check response
     assert response.data == expectedResponse
+
+
+def testGetExplanationByFileAndCoordsSingle(testClient, initDatabaseEmpty):
+    '''
+        Test to check if we can retrieve a single explanation by fileId and coordinates.
+        Attributes:
+            expl1: Explanation that is added to the database.
+            data: The data sent with the request.
+            response: The response of the backend call.
+            expectedResponse: The response we expect to get from the function.
+        Arguments:
+            testClient: The test client we test this for.
+            initDatabaseEmpty: The database instance we test this for.
+    '''
+    del initDatabaseEmpty
+
+    # Create explanation that is going to be added to the database
+    expl1 = Explanations(fileId=63, explId=12, type=0, explanation='This is an explanation',
+                         mistakeText='A mistake', X1=300, X2=350, Y1=100, Y2=120,
+                         replacement1='', replacement2='', replacement3='', feedbackVersion=0.01)
+
+    # Add to the database
+    db.session.add(expl1)
+    db.session.commit()
+
+    # Data for get request
+    data = {
+        'fileId': 63,
+        'x': 320,
+        'y': 110
+    }
+
+    # Retrieve the explanations for the given file and coordinates
+    response = testClient.get('/scoreapi/getExplanationForFileAndCoordinates', query_string=data)
+    # Check if we get the correct status_code:
+    assert response.status_code == 200
+
+    expectedResponse = [{'fileId': 63, 'explId': 12, 'type': 0, 'explanation': 'This is an explanation',
+                        'mistakeText': 'A mistake', 'X1': 300.0, 'X2': 350.0, 'Y1': 100.0, 'Y2': 120.0,
+                         'replacement1': '', 'replacement2': '', 'replacement3': '', 'feedbackVersion': '0.01'}]
+
+    # Check response
+    assert json.loads(response.data) == expectedResponse
+
+
+def testGetExplanationByFileAndCoordsMultiple(testClient, initDatabaseEmpty):
+    '''
+        Test to check if we can retrieve multiple explanations from a single file with matching coordinates.
+        Attributes:
+            expl1, expl2: Explanations that are added to the database.
+            data: The data sent with the request.
+            response: The response of the backend call.
+            expectedResponse: The response we expect to get from the function.
+        Arguments:
+            testClient: The test client we test this for.
+            initDatabaseEmpty: The database instance we test this for.
+    '''
+    del initDatabaseEmpty
+
+    # Create explanation that is going to be added to the database
+    expl1 = Explanations(fileId=10, explId=55, type=0, explanation='This is an explanation',
+                         mistakeText='A mistake', X1=300, X2=350, Y1=100, Y2=120,
+                         replacement1='', replacement2='', replacement3='', feedbackVersion=0.01)
+    expl2 = Explanations(fileId=10, explId=56, type=1, explanation='This is an explanation',
+                         mistakeText='Mistake 2', X1=300, X2=330, Y1=60, Y2=115,
+                         replacement1='Replacement1', replacement2='', replacement3='', feedbackVersion=0.01)
+
+    # Add to the database
+    db.session.add(expl1)
+    db.session.add(expl2)
+    db.session.commit()
+
+    # Data for get request
+    data = {
+        'fileId': 10,
+        'x': 320,
+        'y': 110
+    }
+
+    # Retrieve the explanations for the given file and coordinates
+    response = testClient.get('/scoreapi/getExplanationForFileAndCoordinates', query_string=data)
+    # Check if we get the correct status_code:
+    assert response.status_code == 200
+
+    expectedResponse = [{'fileId': 10, 'explId': 56, 'type': 1, 'explanation': 'This is an explanation',
+                         'mistakeText': 'Mistake 2', 'X1': 300.0, 'X2': 330.0, 'Y1': 60.0, 'Y2': 115.0,
+                         'replacement1': 'Replacement1', 'replacement2': '', 'replacement3': '',
+                         'feedbackVersion': '0.01'},
+                        {'fileId': 10, 'explId': 55, 'type': 0, 'explanation': 'This is an explanation',
+                        'mistakeText': 'A mistake', 'X1': 300.0, 'X2': 350.0, 'Y1': 100.0, 'Y2': 120.0,
+                         'replacement1': '', 'replacement2': '', 'replacement3': '', 'feedbackVersion': '0.01'}]
+
+    # Check response
+    assert json.loads(response.data) == expectedResponse
+
+
+def testGetExplanationByFileAndCoordsError(testClient, initDatabaseEmpty):
+    '''
+        Test to check if we retrieve a 400 error if there does not exist an explanation
+        in the database with the given fileId and coordinates.
+        Attributes:
+            data: The data sent with the request.
+            response: The response of the backend call.
+            expectedResponse: The response we expect to get from the function.
+        Arguments:
+            testClient: The test client we test this for.
+            initDatabaseEmpty: The database instance we test this for.
+    '''
+    del initDatabaseEmpty
+
+    # Data for get request
+    data = {
+        'fileId': 10,
+        'x': 320,
+        'y': 110
+    }
+
+    # Retrieve the explanations for the given file and coordinates
+    response = testClient.get('/scoreapi/getExplanationForFileAndCoordinates', query_string=data)
+    # Check if we get the correct status_code:
+    assert response.status_code == 400
+
+    expectedResponse = b'No explanations found with matching fileId and coordinates'
+
+    # Check response
+    assert response.data == expectedResponse
