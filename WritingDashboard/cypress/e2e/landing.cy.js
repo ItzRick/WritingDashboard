1a1062412b15118f6d027616752ee711ed5adab3
JordyVerhoeven <33899722+JordyVerhoeven@users.noreply.github.com>
2022-07-04 14:40:09 +0200

    Login page tests

--- WritingDashboard/cypress/e2e/landing.cy.js
+++ WritingDashboard/cypress/e2e/landing.cy.js
@@ -1,25 +1,72 @@
-describe('Test the frontpage', () => {
+describe('Test the loginpage', () => {
     beforeEach(() => {
         cy.visit('https://localhost:3000/')
+        cy.get('[id="loginButton"]')
+        .click()
       })
-    it('Checks if the name is on the site', () => {
-        cy.contains('Writing')
-        cy.contains('Dashboard')
-    })
-    it('Checks if the site displays general information', () => {
-        cy.contains('Improve your academic writing.')
-        cy.contains('TU/e students can improve their academic writing.')
-    })
-    it('Checks if we can go to the login page', () => {
-        cy.get('[id="login"]')
-        .click()
+
+    it('Checks if we are on the correct page.', () => {
         cy.url().should('include', '/Login')
-        cy.contains('Log in')
     })
-    it('Checks if we can go to the signup page', () => {
-        cy.get('[id="signup"]')
+
+    it('Checks if all elements are present.', () => {
+        cy.contains('Log in')
+        cy.contains('Note: the TU/e mail is the username of TU/e students.')
+
+        cy.contains('Username:')
+        cy.get('[id="username"]')
+        cy.contains('label', 'Username')
+
+        cy.contains('Password:')
+        cy.get('[id="password"]')
+        cy.contains('label', 'Password')
+    })
+
+    it('Checks if we can add values to the textfields.', () => {
+        cy.get('[id="username"]')
+        .type('test@student.tue.nl')
+        .should('have.value', 'test@student.tue.nl')
+        cy.contains('label', 'Username')
+
+        cy.get('[id="password"]')
+        .type('test')
+        .should('have.value', 'test')
+        cy.contains('label', 'Password')
+    })
+
+    it('Checks if we get an error message.', () => {
+        cy.contains('Username:')
+        cy.get('[id="username"]')
+        .type('test@student.tue.nl')
+        .should('have.value', 'test@student.tue.nl')
+        cy.contains('label', 'Username')
+
+        cy.get('[id="password"]')
+        .type('test')
+        .should('have.value', 'test')
+        cy.contains('label', 'Password')
+
+        cy.get('[id="loginButton"]')
+        .contains('Log in')
         .click()
+        cy.contains('Invalid username and/or password')
+    })
+
+    it('Checks if we can go to the signup page.', () => {
+        cy.contains('here').click()
         cy.url().should('include', '/SignUp')
         cy.contains('Sign Up')
     })
+
+    //Requires the database to have an user with both the username and password set to admin
+    it('Checks whether we can log in.', () => {
+        cy.get('[id="username"]')
+            .type('admin')
+        cy.get('[id="password"]')
+            .type('admin')
+        cy.get('[id="loginButton"]')
+            .click()
+        cy.url().should('include', '/Main')
+    })
+
   })
