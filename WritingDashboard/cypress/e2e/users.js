d2ca2a1f472afa31f9c69b0e999242f4cb11136e
JordyVerhoeven <33899722+JordyVerhoeven@users.noreply.github.com>
2022-07-04 16:29:52 +0200

    Added users test

---
+++ WritingDashboard/cypress/e2e/users.js
@@ -0,0 +1,20 @@
+describe('Test the participants page', () => {
+    beforeEach(() => {
+        cy.visit('https://localhost:3000/Login')
+        cy.get('[id="username"]')
+            .type('admin@tue.nl')
+        cy.get('[id="password"]')
+            .type('AdminPass1')
+        cy.get('[id="login"]').click()
+        cy.get('[id="Users"]').click()
+      })
+
+    it('Checks if all elements are present.', () => {
+        cy.contains('Users')
+        cy.contains('Download user data of selected users')
+        cy.contains('Username')
+        cy.contains('Role')
+        cy.contains('Actions')
+    })
+
+  })
